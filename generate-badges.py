35efd9256ab53d96b04ac9c117b5816ca4d6172f
Nicco Kunzmann <niccokunzmann@rambler.ru>
2017-03-15 20:34:02 +0100

    skipping badges without picture

---
+++ generate-badges.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import os
+
+input_files = [file for file in os.listdir(".")
+               if file.lower().endswith(".csv")]
+
+for input_file in input_files:
+    picture = os.path.splitext(input_file)[0]
+    if not os.path.isfile(picture):
+        print("SKIP: {} has no picture {}".format(input_file, picture))
+        continue
