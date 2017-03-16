f7b494cbee2c933dea38aa817c610ab4d65ee35f
Nicco Kunzmann <niccokunzmann@rambler.ru>
2017-03-16 13:57:57 +0100

    moved installation for travis

--- merge_badges.sh
+++ merge_badges.sh
@@ -5,13 +5,13 @@ cd "`dirname \"$0\"`"
 
 # https://superuser.com/a/381128
 if [ -z "`which rsvg-convert`" ]; then
-  apt-get -y install librsvg2-bin
+  sudo apt-get -y install librsvg2-bin
 fi
 if [ -z "`which python3`" ]; then
-  apt-get -y install python3
+  sudo apt-get -y install python3
 fi
 if [ -z "`which pdftk`" ]; then
-  apt-get -y install pdftk
+  sudo apt-get -y install pdftk
 fi
 
 python3 generate-badges.py
