deb93d986979d1e35cc144ada3be6545570e5e8d
Raghav Jajodia <jajodia.raghav@gmail.com>
2017-09-11 10:23:56 +0530

    Add optional feature to upload PNG as badge background

--- app/main.py
+++ app/main.py
@@ -26,14 +26,27 @@ def upload():
         return redirect(url_for('index'))
 
     file = request.files['file']
-    filename = file.filename
+    filename = file.filename    
+
     # if user does not select file, browser submits an empty part without filename
     if filename == '':
         flash('Please select a CSV file to Upload!', 'error')
         return redirect(url_for('index'))
 
+    # If a PNG is uploaded, push it to the folder
+    if request.files['image'].filename != '':
+        image = request.files['image']
+        imgname = image.filename
+        if '.' in imgname and imgname.rsplit('.', 1)[1] == 'png':
+            imgname = filename.rsplit('.', 1)[0]
+            imgname = secure_filename(imgname)
+            image.save(os.path.join(app.config['UPLOAD_FOLDER'], imgname))
+        else:
+            flash('Please select a PNG image to Upload!', 'error')
+            return redirect(url_for('index'))
+
     if '.' in filename and filename.rsplit('.', 1)[1] == 'csv':
-        filename = secure_filename(file.filename)
+        filename = secure_filename(filename)
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         generate_badges()
         flash('Your Badge has been successfully created!', 'success')
