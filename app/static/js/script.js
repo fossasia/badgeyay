004f85fe153cada38e3b3bb485877c8379c69745
Arjun Nemani <nemaniarjun@gmail.com>
2017-09-15 12:17:47 +0530

    script.js: Changes Dropdown Behaviour
    Earlier to close the links dropbox in the header
    the user needed to click on the glyficon again.
    Now clicking anywhere outside the dropbox will close it.
    
    Fixes #65

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,14 +1,24 @@
 $(function() {
+	
+	$(document).mouseup(function(e) {
+		var container = $(".custom-menu-content");
+		var button = $('.glyphicon-th');
 
-  $('.glyphicon-th').click(function(){
-    $('.custom-menu-content').toggleClass('hidden');
-  });
+	// if the target of the click is not the button, the container or a descendant of the container
+		if (!button.is(e.target) && !container.is(e.target) && container.has(e.target).length === 0) {
+			container.addClass('hidden');
+		}
+	});
 
-  var apiUrl = "https://api.github.com/repos/fossasia/badgeyay/git/refs/heads/development";
-  $.ajax({url: apiUrl, success: function(result){
-    var version = result['object']['sha'];
-    var versionLink = 'https://github.com/fossasia/badgeyay/tree/'+version;
-    var deployLink = $('.version').attr('href', versionLink).html(version);
-  }});
+	$('.glyphicon-th').click(function(){
+		$('.custom-menu-content').toggleClass('hidden');
+	});
+
+	var apiUrl = "https://api.github.com/repos/fossasia/badgeyay/git/refs/heads/development";
+	$.ajax({url: apiUrl, success: function(result){
+		var version = result['object']['sha'];
+		var versionLink = 'https://github.com/fossasia/badgeyay/tree/'+version;
+		var deployLink = $('.version').attr('href', versionLink).html(version);
+	}});
 
 });
