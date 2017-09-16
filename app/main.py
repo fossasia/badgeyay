ff3359cc818e526a39ebb19a79c730efbe6ae77a
Manish Devgan <manish.nsit8@gmail.com>
2017-09-16 16:31:01 +0100

    Creates PDF/ZIP only if user ticks on it.
    merge_badges.py file is taken from https://github.com/fossasia/badgeyay/pull/71 and further tweaking is done on it.
    index.html is changed to be updated with the new changes. and main.py is also changed. style.css is also changed with accordance to it.
    
    resolved conflicts as well
    
    Closes https://github.com/fossasia/badgeyay/issues/66

--- app/main.py
+++ app/main.py
@@ -18,8 +18,13 @@ def index():
     return render_template('index.html')
 
 
-def generate_badges():
-    os.system(SCRIPT)
+def generate_badges(_zip=False,_pdf=False):
+	if _zip == True and _pdf == True:
+		os.system('python merge_badges.py -z -p')
+	elif _zip == True and _pdf == False:
+		os.system('python merge_badges.py -z')
+	else:
+		os.system('python merge_badges.py -p')
 
 
 def empty_directory():
@@ -39,65 +44,75 @@ def empty_directory():
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    empty_directory()
-    filename = "default.png.csv"
-    csv = request.form['csv'].strip()
+	empty_directory()
+	csv = request.form['csv'].strip()
+	img = request.form['img-default']
+	file = request.files['file']
+	_pdf = True if request.form.get('pdf') == 'on' else False
+	_zip = True if request.form.get('zip') == 'on' else False
 
-    # If the textbox is filled
-    if csv != '':
-        f = open(os.path.join(app.config['UPLOAD_FOLDER'], filename), "w+")
-        f.write(csv)
-        f.close()
-    # if user does not select file, browser submits an empty part without filename
-    else:
-        file = request.files['file']
-        if file.filename == '':
-            flash('Please select a CSV file to Upload!', 'error')
-            return redirect(url_for('index'))
-        else:
-            filename = file.filename
+	# If default background is selected
+	if img != '':
+		filename = img + '.csv'
+	# If the textbox is filled
+	elif csv != '':
+		filename = "default.png.csv"
+		f = open(os.path.join(app.config['UPLOAD_FOLDER'], filename), "w+")
+		f.write(csv)
+		f.close()
+	# if user does not select file, browser submits an empty part without filename
+	else:
+		if file.filename == '':
+			flash('Please select a CSV file to Upload!', 'error')
+			return redirect(url_for('index'))
+		else:
+			filename = file.filename
 
-    # If a PNG is uploaded, push it to the folder
-    if request.files['image'].filename != '':
-        image = request.files['image']
-        imgname = image.filename
-        if '.' in imgname and imgname.rsplit('.', 1)[1] == 'png':
-            imgname = filename.rsplit('.', 1)[0]
-            imgname = secure_filename(imgname)
-            image.save(os.path.join(app.config['UPLOAD_FOLDER'], imgname))
-        else:
-            flash('Please select a PNG image to Upload!', 'error')
-            return redirect(url_for('index'))
+	# If a PNG is uploaded, push it to the folder
+	if request.files['image'].filename != '':
+		image = request.files['image']
+		imgname = image.filename
+		if '.' in imgname and imgname.rsplit('.', 1)[1] == 'png':
+			imgname = filename.rsplit('.', 1)[0]
+			imgname = secure_filename(imgname)
+			image.save(os.path.join(app.config['UPLOAD_FOLDER'], imgname))
+		else:
+			flash('Please select a PNG image to Upload!', 'error')
+			return redirect(url_for('index'))
 
-    # if config file is uploaded
-    config_json = request.files['config']
-    if config_json.filename != '':
-        if '.' in config_json.filename and config_json.filename.rsplit('.', 1)[1] == 'json':
-            config_json.save(os.path.join(app.config['UPLOAD_FOLDER'], config_json.filename))
-        else:
-            flash('Only JSON file is accepted!', 'error')
-            return redirect(url_for('index'))
+	# if config file is uploaded
+	config_json = request.files['config']
+	if config_json.filename != '':
+		if '.' in config_json.filename and config_json.filename.rsplit('.', 1)[1] == 'json':
+			config_json.save(os.path.join(app.config['UPLOAD_FOLDER'], config_json.filename))
+		else:
+			flash('Only JSON file is accepted!', 'error')
+			return redirect(url_for('index'))
 
-    # If the csv file is uploaded
-    if '.' in filename and filename.rsplit('.', 1)[1] == 'csv':
-        filename = secure_filename(filename)
-        if filename != "default.png.csv":
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        generate_badges()
+	# If the csv file is uploaded
+	if '.' in filename and filename.rsplit('.', 1)[1] == 'csv':
+		filename = secure_filename(filename)
+		if filename != "default.png.csv":
+			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+		generate_badges(_zip,_pdf)
 
-        # Remove the uploaded files after job in done
-        if filename != "default.png.csv":
-            os.unlink(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        try:
-            os.unlink(os.path.join(app.config['UPLOAD_FOLDER'], imgname))
-        except Exception:
-            traceback.print_exc()
-
-        flash(filename, 'success')
-        return redirect(url_for('index'))
-    else:
-        flash('Only CSV file is accepted!', 'error')
-        return redirect(url_for('index'))
+		# Remove the uploaded files after job in done
+		os.unlink(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+		try:
+			if 'imgname' in locals():
+				os.unlink(os.path.join(app.config['UPLOAD_FOLDER'], imgname))
+		except Exception:
+			traceback.print_exc()
+		if _zip and _pdf:
+			flash(filename, 'success')
+		elif _zip and not _pdf:
+			flash(filename,'success-zip')
+		else:
+			flash(filename,'success-pdf')
+		return redirect(url_for('index'))
+	else:
+		flash('Only CSV file is accepted!', 'error')
+		return redirect(url_for('index'))
 
 
 @app.errorhandler(404)
