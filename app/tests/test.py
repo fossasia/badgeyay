63ae6d1e6f5368b9d41e972dbffa1d62271c338a
nanspro <rahulbishnoi3636@gmail.com>
2017-09-17 22:23:14 +0530

    Less duplication in tests file

--- app/tests/test.py
+++ app/tests/test.py
@@ -5,16 +5,17 @@ import time
 class BadgeyayTest(unittest.TestCase):
 
 	@classmethod
+	def setup(self):
+		self.driver.get('http://localhost:5000')
+		
 	def setUpClass(cls):
 		cls.driver = webdriver.Firefox()
 		super(BadgeyayTest, cls).setUpClass()
 
 	def test_title(self):
-		self.driver.get('http://localhost:5000')
 		self.assertEqual(self.driver.title, 'BadgeYay')
 
 	def test_menu(self):
-		self.driver.get('http://localhost:5000')
 		elem = self.driver.find_element_by_css_selector(".custom-menu-content")
 		self.assertFalse(elem.is_displayed())
 		self.driver.find_element_by_css_selector(".glyphicon-th").click()
@@ -23,7 +24,6 @@ class BadgeyayTest(unittest.TestCase):
 		self.assertFalse(elem.is_displayed())
 
 	def test_error(self):
-		self.driver.get('http://localhost:5000')
 		self.driver.find_element_by_css_selector("form .btn-danger").click()
 		time.sleep(2)
 		success = self.driver.find_element_by_css_selector(".flash-error")
