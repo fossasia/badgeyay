04bea140f0a0a1d72f229eb7edd0876f22b73ef4
Raghav Jajodia <jajodia.raghav@gmail.com>
2017-09-18 09:47:08 +0530

    Fixes the design and adds test (#106)
    * Fix styling of the board
    
    * Reduce the size of issue template
    
    * Reduce the size of PR template
    
    * Add upload test

--- app/tests/test.py
+++ app/tests/test.py
@@ -1,13 +1,15 @@
 from selenium import webdriver
 import unittest
 import time
+import os
 
 class BadgeyayTest(unittest.TestCase):
-
+	
 	@classmethod
 	def setup(self):
 		self.driver.get('http://localhost:5000')
 		
+	@classmethod
 	def setUpClass(cls):
 		cls.driver = webdriver.Firefox()
 		super(BadgeyayTest, cls).setUpClass()
@@ -16,29 +18,33 @@ class BadgeyayTest(unittest.TestCase):
 		self.assertEqual(self.driver.title, 'BadgeYay')
 
 	def test_menu_visibility(self):
-		#elem is div element of right most menu bar ,until glyphicon is not clicked it should not
-		#display all menus like (code, bug report etc)
+		# Check that menu is not visible initially
 		elem = self.driver.find_element_by_css_selector(".custom-menu-content")
 		self.assertFalse(elem.is_displayed())
 	
 	def test_menu_click_on(self):
-		#elem is glyphicon element, when it is clicked it should display menu bar containing
-		#(code,bug reports etc)
+		# Check that menu is visible when opened
 		elem=self.driver.find_element_by_css_selector(".glyphicon-th").click()
 		self.assertTrue(elem.is_displayed())
 	
 	def test_menu_click_off(self):
-		#elem is glyphicon element, when it is clicked again it should hide menu bar containing
-		#(code,bug reports etc)		
+		# Check that menu is not visible when closed
 		elem=self.driver.find_element_by_css_selector(".glyphicon-th").click()
 		self.assertFalse(elem.is_displayed())
 
 	def test_error(self):
-		self.driver.find_element_by_css_selector("form .btn-danger").click()
+		self.driver.find_element_by_css_selector("form .btn-primary").click()
 		time.sleep(2)
 		success = self.driver.find_element_by_css_selector(".flash-error")
 		self.assertIn(u'Please select a CSV file to Upload!', success.text)
 
+	def test_csv_upload(self):
+		Imagepath = os.path.abspath(os.path.join(os.getcwd(), 'sample/vip.png.csv'))
+		self.driver.find_element_by_name("file").send_keys(Imagepath)
+		self.driver.find_element_by_css_selector("form .btn-primary").click()
+		time.sleep(3)
+		success = self.driver.find_element_by_css_selector(".flash-success")
+		self.assertIn(u'Your badges has been successfully generated!', success.text)
 
 	@classmethod
 	def tearDownClass(cls):
