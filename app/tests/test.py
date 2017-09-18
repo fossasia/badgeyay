6e14bafc12807ed0f7816b40357a83aa8bf93383
Raghav Jajodia <jajodia.raghav@gmail.com>
2017-09-18 22:57:53 +0530

    Added few more tests

--- app/tests/test.py
+++ app/tests/test.py
@@ -33,9 +33,29 @@ class BadgeyayTest(unittest.TestCase):
 		success = self.driver.find_element_by_css_selector(".flash-error")
 		self.assertIn(u'Please select a CSV file to Upload!', success.text)
 
+	def test_select_background_upload(self):
+		CSVpath = os.path.abspath(os.path.join(os.getcwd(), 'sample/vip.png.csv'))
+		self.driver.find_element_by_name("file").send_keys(CSVpath)
+		self.driver.find_element_by_css_selector(".btn-group .dropdown-toggle").click()
+		self.driver.find_element_by_css_selector("li[data-item='team.png']").click()
+		self.driver.find_element_by_css_selector("form .btn-primary").click()
+		time.sleep(3)
+		success = self.driver.find_element_by_css_selector(".flash-success")
+		self.assertIn(u'Your badges has been successfully generated!', success.text)
+
+	def test_png_upload(self):
+		Imagepath = os.path.abspath(os.path.join(os.getcwd(), 'badges/badge_1.png.csv'))
+		CSVpath = os.path.abspath(os.path.join(os.getcwd(), 'sample/vip.png.csv'))
+		self.driver.find_element_by_name("file").send_keys(CSVpath)
+		self.driver.find_element_by_name("image").send_keys(Imagepath)
+		self.driver.find_element_by_css_selector("form .btn-primary").click()
+		time.sleep(3)
+		success = self.driver.find_element_by_css_selector(".flash-success")
+		self.assertIn(u'Your badges has been successfully generated!', success.text)
+
 	def test_csv_upload(self):
-		Imagepath = os.path.abspath(os.path.join(os.getcwd(), 'sample/vip.png.csv'))
-		self.driver.find_element_by_name("file").send_keys(Imagepath)
+		CSVpath = os.path.abspath(os.path.join(os.getcwd(), 'sample/vip.png.csv'))
+		self.driver.find_element_by_name("file").send_keys(CSVpath)
 		self.driver.find_element_by_css_selector("form .btn-primary").click()
 		time.sleep(3)
 		success = self.driver.find_element_by_css_selector(".flash-success")
