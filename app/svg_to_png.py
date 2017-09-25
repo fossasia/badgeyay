e2e11d62bf30740fea08192b8c3908e5be147574
rupav jain <rupavwinchester@gmail.com>
2017-09-25 10:32:29 +0530

    Modify text/header
    remove_trailing_spaces.

--- app/svg_to_png.py
+++ app/svg_to_png.py
@@ -6,26 +6,25 @@ APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 SVGS_FOLDER = os.path.join(APP_ROOT, 'static/svgs')
 UPLOAD_FOLDER = os.path.join(APP_ROOT, 'static/uploads')
 
-def do_svg2png(filename,opacity,fill,text):
+def do_svg2png(filename,opacity,fill,text_):
     png_filename = filename
     filename = filename.rsplit(".",1)[0] + '.svg'
     filename = os.path.join(SVGS_FOLDER, filename)
     tree = etree.parse(open(filename, 'r'))
-    for element in tree.iter():
-        if element.tag.split("}")[1] == "rect":
-            if element.get("id") == "rect4504":
-                style_detail = element.get("style")
-                style_detail = style_detail.split(";")
-                style_detail[0] = "opacity:"+str(opacity)
-                style_detail[1] = "fill:"+str(fill)
-                style_detail = ';'.join(style_detail)
-                element.set("style", style_detail)
-
-        if element.tag.split("}")[1] == "tspan":
-            if element.get("id") == "tspan932":
-                #Code to change the text in SVG
-                #Change value of tspan932 to function argument text
-                print("Write Code")
-
+    element = tree.getroot()
+    #changing style using XPath.
+    path = element.xpath('//*[@id="rect4504"]')[0]
+    style_detail = path.get("style")
+    style_detail = style_detail.split(";")
+    style_detail[0] = "opacity:"+str(opacity)
+    style_detail[1] = "fill:"+str(fill)
+    style_detail = ';'.join(style_detail)
+    path.set("style", style_detail)
+    #changing text using XPath.
+    path = element.xpath('//*[@id="tspan932"]')[0]
+    path.text = text_
+    #Saving in the original XML tree
+    etree.ElementTree(element).write(filename, pretty_print=True)
+    print("done")
     svg2png(url=filename,write_to=UPLOAD_FOLDER+'/'+png_filename)
     print("Image Saved")
