b379c7247ed8441c512ff73d3ca9d4e1d59aeb73
devshiva619 <deveshverma619@gmail.com>
2017-10-08 14:47:56 +0530

    rectified errors in merge_badges_v2

--- app/merge_badges_v2.py
+++ app/merge_badges_v2.py
@@ -1,6 +1,7 @@
 #!usr/bin/python3
 import os
 import argparse
+import subprocess
 from PyPDF2 import PdfFileMerger
 from exceptions import PackageNotFoundError
 
@@ -12,15 +13,15 @@ arguments = parser.parse_args()
 _pdf = arguments.pdf
 _zip = arguments.zip
 
-if os.system('which rsvg-convert') != 0:
+if subprocess.run('which rsvg-convert') != 0:
     raise PackageNotFoundError("Package rsvg-convert not found")
-if os.system('which python3') != 0:
+if subprocess.run('which python3') != 0:
     raise PackageNotFoundError("Package python3 not found")
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 BADGES_FOLDER = os.path.join(APP_ROOT, 'static/badges')
 
-os.system('python3 ' + APP_ROOT + '/generate-badges.py')
+subprocess.run('python3 ' + APP_ROOT + '/generate-badges.py')
 
 input_folders = [file for file in os.listdir(BADGES_FOLDER) if file.lower().endswith(".badges")]
 
@@ -32,7 +33,7 @@ def generate_pdfs(folder_path):
         pdf_path = os.path.splitext(svg_path)[0] + '.pdf'
         print('svg: {}'.format(svg_path))
         print('pdf: {}'.format(pdf_path))
-        os.system('rsvg-convert -f pdf -o {} {}'.format(pdf_path, svg_path))
+        subprocess.run('rsvg-convert -f pdf -o {} {}'.format(pdf_path, svg_path))
 
 
 # Generating PDF files from svg.
@@ -71,7 +72,7 @@ if _zip:
     print("Created {}".format(final_path))
     print("Generating ZIP file")
 
-    os.system('find static/badges/ \( -iname \*.svg -o -iname \*.pdf \) | zip  -@ static/badges/all-badges.zip')
+    subprocess.run('find static/badges/ \( -iname \*.svg -o -iname \*.pdf \) | zip  -@ static/badges/all-badges.zip')
 
     print('Generating ZIP of SVG files')
 
@@ -83,4 +84,4 @@ if _zip:
     for folder in input_folders:
         folder_path = os.path.join(BADGES_FOLDER, folder)
         file_name = folder_path + '.svg.zip'
-        os.system('find ' + folder_path + ' -type f -name *.svg | zip -@ ' + file_name)
+        subprocess.run('find ' + folder_path + ' -type f -name *.svg | zip -@ ' + file_name)
