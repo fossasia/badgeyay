78e83af88c7f708a13d4f82f6c0c75d9dedcc107
Vaibhav Singh <vaibhav.singh.14cse@bml.edu.in>
2017-11-15 02:25:46 +0530

    Improved Upload Button, typo fix and refactored code

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,51 +1,52 @@
-$(document).on("ready", function() {
+/*jshint esversion: 6 */
+$(document).on("ready", function () {
 
-	$(document).mouseup(function(e) {
-		var container = $(".custom-menu-content");
-		var button = $(".glyphicon-th");
-		// if the target of the click is not the button,
-		// the container or a descendant of the container
-		if (!button.is(e.target) && !container.is(e.target) && container.has(e.target).length === 0) {
-			container.addClass("hidden");
-		}
-	});
+    $(document).mouseup(function (e) {
+        var container = $(".custom-menu-content");
+        var button = $(".glyphicon-th");
+        // if the target of the click is not the button,
+        // the container or a descendant of the container
+        if (!button.is(e.target) && !container.is(e.target) && container.has(e.target).length === 0) {
+            container.addClass("hidden");
+        }
+    });
 
-	$(".glyphicon-th").click(function(){
-		$(".custom-menu-content").toggleClass("hidden");
-	});
+    $(".glyphicon-th").click(function () {
+        $(".custom-menu-content").toggleClass("hidden");
+    });
 
-	$(".menu-options").click(function(){
-		var i = $(this).data("item");
-		$(".placeholder").text(i);
-		$("input[name='img-default']").val(i).trigger('change');
-	});
-	
-	
-	$("#picker").minicolors({
-		control: "hue",		
-		format: "hex",
-		defaultValue: '',
-		letterCase: "lowercase",
-		position: "bottom left",
-		theme: "bootstrap"
-	});
-	
-	
-	var apiUrl = "https://api.github.com/repos/fossasia/badgeyay/git/refs/heads/development";
-	$.ajax({
-		url: apiUrl,
-		async: true,
-		success(result) {
-			if(typeof result.object !== "undefined" && typeof result.object.sha !== "undefined") {
-				var version = result["object"]["sha"];
-				var versionLink = "https://github.com/fossasia/badgeyay/tree/"+version;
-				var deployLink = $(".version").attr("href", versionLink).html(version);
-			} else {
-				$(".version").html("Failed to access version");
-			}
-		},
-		error(error) {
-			$(".version").html("Failed to access version");
-		}
-	});
+    $(".menu-options").click(function () {
+        var i = $(this).data("item");
+        $(".placeholder").text(i);
+        $('input[name=\'img-default\']').val(i).trigger('change');
+    });
+
+
+    $("#picker").minicolors({
+        control: 'hue',
+        format: 'hex',
+        defaultValue: '',
+        letterCase: 'lowercase',
+        position: 'bottom left',
+        theme: 'bootstrap'
+    });
+
+
+    var apiUrl = "https://api.github.com/repos/fossasia/badgeyay/git/refs/heads/development";
+    $.ajax({
+        url: apiUrl,
+        async: true,
+        success(result) {
+            if (typeof result.object !== "undefined" && typeof result.object.sha !== "undefined") {
+                var version = result.object.sha;
+                var versionLink = "https://github.com/fossasia/badgeyay/tree/" + version;
+                var deployLink = $(".version").attr("href", versionLink).html(version);
+            } else {
+                $(".version").html("Failed to access version");
+            }
+        },
+        error(error) {
+            $(".version").html("Failed to access version");
+        }
+    });
 });
