03b2fba96ad617be7fd1dd5d6d3c6403ceeeec00
rupav jain <rupavwinchester@gmail.com>
2018-01-28 18:20:53 +0530

    Add color change option of details in badges (#522)
    Add `do_text_fill` function
    
    Add static color picker functionality for text-fill
    
    Allow text-fill option
    
    Now one can change text color on badges when needed.
    
    Changes done in back-end and front-end folder to add text color option
    
    Help in heroku deploy
    
    removed spaces for proper indentation of badge details
    
    Revert "Help in heroku deploy"
    
    This reverts commit a9255457aa09f6d698ea7171c9090bda561a88d7.
    
    Revert "Allow text-fill option"
    
    This reverts commit 03d603ffe2cb70e3ee6f9c60bbb5df1bdb29fe88.
    
    Revert "Add static color picker functionality for text-fill"
    
    This reverts commit 87d1a599bd9f9f2874536be7d17131d2e4ab19cf.
    
    Revert "Add `do_text_fill` function"
    
    This reverts commit ca42327ff7a29105429a8cdaf8fb22fd373267d0.
    
    Revert "Remove spaces to correct indent of text in badges"
    
    This reverts commit 3e530999864777046ecd4e34e80ce4e2d2cd3fc1.
    
    Remove modifications in js files in backend folder.

--- backend/app/svg_to_png.py
+++ backend/app/svg_to_png.py
@@ -6,7 +6,7 @@ import os
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 SVGS_FOLDER = os.path.join(APP_ROOT, 'static/svgs')
 UPLOAD_FOLDER = os.path.join(APP_ROOT, 'static/uploads')
-
+ids = ['text4611', 'text4585', 'text4559', 'text4533', 'text4399', 'text4373', 'text4347', 'text4313']
 
 def do_svg2png(filename, opacity, fill):
     """
@@ -36,3 +36,38 @@ def do_svg2png(filename, opacity, fill):
     print("done")
     svg2png(url=filename, write_to=UPLOAD_FOLDER + '/' + png_filename)
     print("Image Saved")
+
+
+def do_text_fill(filename, fill):
+    """
+    Module to change color of badge's details
+    :param `filename` - svg file to modify.
+    :param `fill` - color to be applied on text
+    """
+    tree = etree.parse(open(filename, 'r'))
+    element = tree.getroot()
+    for _id in ids:
+        path = element.xpath(("//*[@id='{}']").format(_id))[0]
+        style_detail = path.get("style")
+        style_detail = style_detail.split(";")
+        if style_detail[7].split(':')[0] == 'fill':
+            style_detail[7] = "fill:" + str(fill)
+            print(style_detail[7])
+        elif style_detail[6].split(':')[0] == 'fill':
+            style_detail[6] = "fill:" + str(fill)
+            print(style_detail[6])
+        else:
+            for ind, i in enumerate(style_detail):
+                if i.split(':')[0] == 'fill':
+                    style_detail[ind] = "fill:" + str(fill)
+        style_detail = ';'.join(style_detail)
+        text_nodes = path.getchildren()
+        path.set("style", style_detail)
+        for t in text_nodes:
+            text_style_detail = t.get("style")
+            text_style_detail = text_style_detail.split(";")
+            text_style_detail[-1] = "fill:" + str(fill)
+            text_style_detail = ";".join(text_style_detail)
+            t.set("style", text_style_detail)
+    etree.ElementTree(element).write(filename, pretty_print=True)
+    print("Text Fill saved!")
