2182895b8ab81fc3a2913b74ef3bbfd5daf23c34
Parth <24358501+ParthS007@users.noreply.github.com>
2018-01-29 23:05:52 +0530

    Add CNAME in gh-pages deployment (#541)

--- frontend/config/deploy.js
+++ frontend/config/deploy.js
@@ -6,15 +6,22 @@ module.exports = function(deployTarget) {
     // include other plugin configuration that applies to all deploy targets here
   };
 
+  ENV.pipeline = {
+    disabled: {
+      git: true
+    }
+  };
 
-if (deployTarget === 'gh-pages') {
-     ENV.build.environment = 'production'; 
-     ENV.git = {
-       repo: `https://ParthS007:${process.env.GH_TOKEN}@github.com/fossasia/badgeyay`,
-       branch: 'gh-pages',
-       commitMessage: 'Deployed %@'
-     };
-   }
+  if (deployTarget === 'gh-pages-with-domain' || deployTarget === 'gh-pages') {
+    ENV.pipeline = {
+      disabled: {}
+    };
+    ENV.git = {
+      repo: `https://ParthS007:${process.env.GH_TOKEN}@github.com/fossasia/badgeyay`,
+      branch: 'gh-pages',
+      commitMessage: 'Travis CI Clean Deploy %@'
+    };
+  }
 
   return ENV;
 };
