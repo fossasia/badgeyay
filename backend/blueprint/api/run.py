79fd20a66d125fac382175ae8334e59c9fe668ca
Manish Devgan <manish.nsit8@gmail.com>
2018-05-03 23:56:16 +0530

    Adds ErrorHandlers to API (#681)

--- backend/blueprint/api/run.py
+++ backend/blueprint/api/run.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from controllers import generateBadges
 from controllers import homePage
+from controllers import errorHandlers
 
 app = Flask(__name__)
 
 
 app.register_blueprint(generateBadges.router, url_prefix="/api")
 app.register_blueprint(homePage.router)
+app.register_blueprint(errorHandlers.router)
 
 
 app.run()
