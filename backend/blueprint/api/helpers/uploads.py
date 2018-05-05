5f6efab5e12d351558e2fd722c61d763dfe64653
Manish Devgan <manish.nsit8@gmail.com>
2018-05-06 03:46:02 +0530

    Adds Image Upload API
    Adds Image Upload API to new backend
    
    Cheers!

---
+++ backend/blueprint/api/helpers/uploads.py
@@ -0,0 +1,21 @@
+import uuid
+import os
+from flask import current_app as app
+
+
+def generateFileName():
+    return str(uuid.uuid4())
+
+def saveToImage(imageFile=None, extension='.png'):
+    imageName = generateFileName() + extension
+    imageDirectory = os.path.join(app.config.get('BASE_DIR'), 'static', 'uploads', 'image')
+
+    if not os.path.isdir(imageDirectory):
+        os.makedirs(imageDirectory)
+
+    imagePath = os.path.join(imageDirectory, imageName)
+    image = open(imagePath, "wb")
+    image.write(imageFile.decode('base64'))
+    image.close()
+
+    return imageName
