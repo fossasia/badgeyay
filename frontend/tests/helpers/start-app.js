3b0931be46751a1f116de8a63c0f8635da9ffbad
ParthS007 <parth1989shandilya@gmail.com>
2018-05-13 03:11:57 +0530

    Adds Plugins and setup for modularising frontend

--- frontend/tests/helpers/start-app.js
+++ frontend/tests/helpers/start-app.js
@@ -1,11 +1,11 @@
+import Ember from 'ember';
 import Application from '../../app';
 import config from '../../config/environment';
-import { merge } from '@ember/polyfills';
-import { run } from '@ember/runloop';
+
+const { merge, run } = Ember;
 
 export default function startApp(attrs) {
   let attributes = merge({}, config.APP);
-  attributes.autoboot = true;
   attributes = merge(attributes, attrs); // use defaults, but you can override;
 
   return run(() => {
