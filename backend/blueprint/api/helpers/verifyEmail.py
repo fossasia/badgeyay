bb749fed7b8ca6478b608c67a7d4eabdd48e34c1
Vaibhav Singh <vaibhav.singh.14cse@bml.edu.in>
2018-05-13 17:45:33 +0530

    Fixes #750: Added Email Verification

---
+++ backend/blueprint/api/helpers/verifyEmail.py
@@ -0,0 +1,10 @@
+import re
+from flask import jsonify
+
+
+def verifyEmail(email):
+    match = re.match('^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$', email)
+    if match == None:
+        return jsonify(
+            Response(403).generateMessage("Not an Email Address")
+
