e8470cd607358cd633040d45decc542c029d6ccd
ParthS007 <parth1989shandilya@gmail.com>
2018-05-15 21:20:56 +0530

    Added Support for Python SDK

--- backend/blueprint/api/run.py
+++ backend/blueprint/api/run.py
@@ -1,7 +1,5 @@
-import os
-from flask import Flask
 from flask_migrate import Migrate
-
+from api import create_app
 from api.db import db
 from api.config import config
 from api.controllers import (
@@ -15,15 +13,7 @@ from api.controllers import (
     registerUser
 )
 
-
-
-app = Flask(__name__, instance_relative_config=True)
-app.config['BASE_DIR'] = os.path.dirname(os.path.abspath(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % config.POSTGRES
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-app.config['SECRET_KEY'] = config.POSTGRES['secret']
-app.config['DEBUG'] = config.DEBUG
-app.config.from_object('api.config.mailConfig.MailConfig')
+app = create_app()
 
 db.init_app(app)
 migrate = Migrate(app, db)
