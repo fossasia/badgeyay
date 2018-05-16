e43ee7f8f6998b326d2d81d9a383572af19127e9
Parth Shandilya <parth1989shandilya@gmail.com>
2018-05-17 02:02:06 +0530

    Added Support for Python SDK (#765)

---
+++ backend/blueprint/api/__init__.py
@@ -0,0 +1,19 @@
+import os
+from flask import Flask
+import firebase_admin
+from firebase_admin import credentials
+from api.config import config
+
+
+def create_app():
+    app = Flask(__name__, instance_relative_config=True)
+    cred = credentials.Certificate('Enter the path to ServiceAccountkey.json')
+    firebase_admin.initialize_app(cred)
+    app.config['BASE_DIR'] = os.path.dirname(os.path.abspath(__file__))
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % config.POSTGRES
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+    app.config['SECRET_KEY'] = config.POSTGRES['secret']
+    app.config['DEBUG'] = config.DEBUG
+    app.config.from_object('api.config.mailConfig.MailConfig')
+
+    return app
