3efb6a05e5e07cd6eaa77211ee63796ab9b5fbf6
yashLadha <201551061@iiitvadodara.ac.in>
2018-05-16 18:06:11 +0530

    Fixes #771 : User ember data model in frontend

--- frontend/app/routes/index.js
+++ frontend/app/routes/index.js
@@ -1,4 +1,16 @@
+import DS from 'ember-data';
 import Route from '@ember/routing/route';
 
+const { Store } = DS;
+
 export default Route.extend({
+  actions: {
+    createUser(uid, name) {
+      let user = Store.createRecord('user', {
+        uid,
+        name
+      });
+      user.save();
+    }
+  }
 });
