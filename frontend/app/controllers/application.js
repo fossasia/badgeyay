f7defec54d0a359642bbe96b9710d89b8f662ea2
ParthS007 <parth1989shandilya@gmail.com>
2018-05-21 00:29:56 +0530

    Added Create Badges Route and moved Data components in it.

--- frontend/app/controllers/application.js
+++ frontend/app/controllers/application.js
@@ -1,19 +1,4 @@
 import Controller from '@ember/controller';
-import { inject as service } from '@ember/service';
 
 export default Controller.extend({
-  routing : service('-routing'),
-  actions : {
-    mutateCSV(csvData) {
-      let csv_ = this.get('store').createRecord('csv-file', {
-        csvFile   : csvData,
-        extension : 'csv'
-      });
-      csv_.save();
-    },
-
-    mutateText(txtData) {
-      console.log(txtData);
-    }
-  }
 });
