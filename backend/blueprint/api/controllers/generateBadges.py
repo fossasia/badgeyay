c1b01d23536808b3e371ef6b1b4ddb464bd7583d
Manish Devgan <manish.nsit8@gmail.com>
2018-05-22 02:00:15 +0530

    Adds Badges to Database
    fix

--- backend/blueprint/api/controllers/generateBadges.py
+++ backend/blueprint/api/controllers/generateBadges.py
@@ -5,7 +5,8 @@ from flask import current_app as app
 from api.utils.response import Response
 from api.utils.svg_to_png import SVG2PNG
 from api.utils.merge_badges import MergeBadges
-
+from api.models.badges import Badges
+from api.models.user import User
 
 router = Blueprint('generateBadges', __name__)
 
@@ -35,6 +36,11 @@ def generateBadges():
     svg2png.do_text_fill('static/badges/8BadgesOnA3.svg', text_color)
     merge_badges = MergeBadges(image_name, csv_name)
     merge_badges.merge_pdfs()
+
+    uid = data.get('uid')
+    user_creator = User.getUser(user_id=uid)
+    badge_created = Badges(image=image_name, csv=csv_name, text_color=text_color, badge_size='A3', creator=user_creator)
+    badge_created.save_to_db()
 
     output = os.path.join(app.config.get('BASE_DIR'), 'static', 'temporary', image_name)
 
