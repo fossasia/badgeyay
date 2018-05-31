71e5215a2ffd169925f56eda81814cd8ee1aad4b
abhijeet <abhijeet40308@gmail.com>
2018-05-31 15:11:07 +0530

    Fixes #888 : Enhanced font when slider is dragged

--- frontend/app/components/text-component/font-size.js
+++ frontend/app/components/text-component/font-size.js
@@ -3,10 +3,21 @@ import Component from '@ember/component';
 export default Component.extend({
   init() {
     this.defFontSize = '';
+    this.textZoom = false;
     this._super(...arguments);
   },
 
   input(defFontSize) {
-    this.get('defFontSize', defFontSize);
+    console.log('DefFontSize', this.defFontSize);
+  },
+
+  mouseUp() {
+    console.log('Mouse up');
+    this.set('textZoom', false);
+  },
+
+  mouseDown() {
+    console.log('Mouse down');
+    this.set('textZoom', true);
   }
 });
