470a513748ea6b6bd32b07ea2e81add4f8525a17
Manish Devgan <manish.nsit8@gmail.com>
2018-06-01 22:22:06 +0530

    Adds Schema + API fo gettin all generated badges
    Adds Schema + API fo gettin all generated badges

--- api/schemas/badges.py
+++ api/schemas/badges.py
@@ -22,3 +22,25 @@ class BadgeSchema(Schema):
         include_resource_linkage=True,
         type_='User'
     )
+
+
+class AllBadges(Schema):
+    class Meta:
+        type_ = 'all-badges'
+        self_view = 'admin.get_all_badges'
+        kwargs = {'id': '<id>'}
+
+    id = fields.Str(required=True, dump_only=True)
+    image = fields.Str(required=True)
+    csv = fields.Str(required=True)
+    badge_id = fields.Str(required=True)
+    text_color = fields.Str(required=True)
+    badge_size = fields.Str(required=True)
+    user_id = fields.Relationship(
+        self_url='/api/upload/get_file',
+        self_url_kwargs={'file_id': '<id>'},
+        related_url='/user/register',
+        related_url_kwargs={'id': '<id>'},
+        include_resource_linkage=True,
+        type_='User'
+    )
