4154c23fe2db969781b2cef5caf1f560a8db83c7
Parth Shandilya <parth1989shandilya@gmail.com>
2018-06-01 11:55:54 +0530

    Added Badge Data Model and Serializer in the frontend (#901)
    <!-- Add the issue number that is fixed by this PR (In the form Fixes #123) -->
    Fixes #893 
    
    #### Checklist
    
    - [x] I have read the [Contribution & Best practices Guide](https://blog.fossasia.org/open-source-developer-guide-and-best-practices-at-fossasia) and my PR follows them.
    - [x] My branch is up-to-date with the Upstream `development` branch.
    - [x] I have added necessary documentation (if appropriate)
    
    #### Changes proposed in this pull request:
    
    - Added Badge Data Model and Serializer in the frontend

---
+++ frontend/app/models/badge.js
@@ -0,0 +1,13 @@
+import DS from 'ember-data';
+
+const { Model, attr } = DS;
+
+export default Model.extend({
+  uid        : attr('string'),
+  csv        : attr('string'),
+  image      : attr('string'),
+  font_type  : attr('string'),
+  font_color : attr('string'),
+  font_size  : attr('string'),
+  badge_size : attr('string')
+});
