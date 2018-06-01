bec85f33aeb51b239f4b96f69d721596a1a0eb37
Vaibhav Singh <vaibhav.singh.14cse@bml.edu.in>
2018-06-01 22:36:00 +0530

    Fixes #704: Added Config.p - Added .env file to store configurations - Updated requirements

--- api/__init__.py
+++ api/__init__.py
@@ -4,6 +4,25 @@ import firebase_admin
 from firebase_admin import credentials
 from api.config import config
 from flask_cors import CORS
+from flask_migrate import Migrate
+from api.db import db
+from dotenv import load_dotenv, find_dotenv
+from api.controllers import (
+    generateBadges,
+    homePage,
+    errorHandlers,
+    loginUser,
+    fileUploader,
+    modifyUser,
+    resetUser,
+    registerUser,
+    oauthToken,
+    assetHelper,
+    admin
+)
+
+
+load_dotenv(find_dotenv())
 
 
 def create_app():
@@ -18,8 +37,25 @@ def create_app():
     else:
         app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-    app.config['SECRET_KEY'] = config.POSTGRES['secret']
-    app.config['DEBUG'] = config.DEBUG
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+    app.config['DEBUG'] = os.getenv('DEBUG')
+    app.config.from_object('api.config.mailConfig.MailConfig')
+    app.config.from_object(os.getenv('APP_CONFIG', default='api.config.config.ProductionConfig'))
     CORS(app, resources={r"*": {"origins": "*"}})
+    db.init_app(app)
+    Migrate(app, db)
+
+    with app.app_context():
+        app.register_blueprint(generateBadges.router, url_prefix='/api')
+        app.register_blueprint(registerUser.router, url_prefix='/user')
+        app.register_blueprint(loginUser.router, url_prefix='/user')
+        app.register_blueprint(fileUploader.router, url_prefix='/api/upload')
+        app.register_blueprint(modifyUser.router, url_prefix='/user/change')
+        app.register_blueprint(homePage.router)
+        app.register_blueprint(errorHandlers.router)
+        app.register_blueprint(assetHelper.router, url_prefix='/api')
+        app.register_blueprint(resetUser.router, url_prefix='/reset')
+        app.register_blueprint(oauthToken.router, url_prefix='/api')
+        app.register_blueprint(admin.router, url_prefix='/admin')
 
     return app
