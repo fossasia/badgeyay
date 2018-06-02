d6c2cb6f2d6810d0f34d9210f38db2d090a12041
Manish Devgan <manish.nsit8@gmail.com>
2018-06-02 15:31:54 +0530

    Fixes #903 : Adds Schema + API fo getting all generated badges (#904)
    <!-- Add the issue number that is fixed by this PR (In the form Fixes #123) -->
    Fixes #903 
    
    #### Checklist
    
    - [x] I have read the [Contribution & Best practices Guide](https://blog.fossasia.org/open-source-developer-guide-and-best-practices-at-fossasia) and my PR follows them.
    - [x] My branch is up-to-date with the Upstream `development` branch.
    - [ ] I have added necessary documentation (if appropriate)
    
    ### Preview Link 
    
    - **Replace XXX with your PR no**
    - Link to live demo: http://pr-XXX-fossasia-badgeyay.surge.sh  
    
    #### Changes proposed in this pull request:
    
    - Adds AllBadges Schema
    - Adds route for getting all the generated badges.
    
    Cheers!

--- api/controllers/admin.py
+++ api/controllers/admin.py
@@ -1,6 +1,8 @@
 from flask import jsonify, Blueprint
 from api.models.user import User
+from api.models.badges import Badges
 from api.schemas.user import AllUsersSchema
+from api.schemas.badges import AllBadges
 
 
 router = Blueprint('admin', __name__)
@@ -11,4 +13,12 @@ def show_all_users():
     users = User.query.all()
     schema = AllUsersSchema(many=True)
     result = schema.dump(users)
+    return jsonify(result.data)
+
+
+@router.route('/get_all_badges', methods=['GET'])
+def get_all_badges():
+    all_badges = Badges.query.all()
+    schema = AllBadges(many=True)
+    result = schema.dump(all_badges)
     return jsonify(result.data)
