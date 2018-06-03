3329b240c1cce241b130f3c25b6e5abc619fef91
Manish Devgan <manish.nsit8@gmail.com>
2018-06-03 12:43:42 +0530

    Revert "Fixes #437: Added Swagger UI" (#917)
    Reverts fossasia/badgeyay#749

--- api/controllers/homePage.py
+++ api/controllers/homePage.py
@@ -6,15 +6,6 @@ router = Blueprint('homePage', __name__)
 
 @router.route('/', methods=['POST'])
 def homePage():
-    """
-    Return API status.
-    ---
-    tags:
-      - HomePage
-    responses:
-      200:
-        description: This is the homepage route. Please follow /api/generate_badges for badge creation
-    """
     return jsonify(
         Response(200).generateMessage(
             'This is the homepage route. Please follow /api/generate_badges for badge creation'))
