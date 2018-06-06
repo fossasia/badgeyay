91c7c690436ddc7d6efd28c8966ea1cfaa30c846
Manish Devgan <manish.nsit8@gmail.com>
2018-06-06 09:40:25 +0530

    Fixes #948 : Adds get_badges functionality per user (#949)
    <!-- Add the issue number that is fixed by this PR (In the form Fixes #123) -->
    Fixes #948 
    
    #### Checklist
    
    - [X] I have read the [Contribution & Best practices Guide](https://blog.fossasia.org/open-source-developer-guide-and-best-practices-at-fossasia) and my PR follows them.
    - [X] My branch is up-to-date with the Upstream `development` branch.
    - [ ] I have added necessary documentation (if appropriate)
    
    ### Preview Link 
    
    - **Replace XXX with your PR no**
    - Link to live demo: http://pr-XXX-fossasia-badgeyay.surge.sh  
    
    #### Changes proposed in this pull request:
    
    - Adds get_badges route in backend.
    
    @djmgit @yashLadha @ParthS007 please review and merge

--- api/schemas/badges.py
+++ api/schemas/badges.py
@@ -44,3 +44,25 @@ class AllBadges(Schema):
         include_resource_linkage=True,
         type_='User'
     )
+
+
+class UserBadges(Schema):
+    class Meta:
+        type_ = 'user-badges'
+        self_view = 'generateBadges.get_badges'
+        kwargs = {'id': '<id>'}
+
+    id = fields.Str(required=True, dump_only=True)
+    image = fields.Str(required=True)
+    csv = fields.Str(required=True)
+    badge_id = fields.Str(required=True)
+    text_color = fields.Str(required=True)
+    badge_size = fields.Str(required=True)
+    user_id = fields.Relationship(
+        self_url='/api/upload/get_file',
+        self_url_kwargs={'file_id': '<id>'},
+        related_url='/user/register',
+        related_url_kwargs={'id': '<id>'},
+        include_resource_linkage=True,
+        type_='User'
+    )
