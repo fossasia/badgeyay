a725f88aa3a9082e27b5c8be0b520f48a02fcc5b
Yash Ladha <201551061@iiitvadodara.ac.in>
2018-06-08 20:52:43 +0530

    Fixes #959 : Updating the user profile and storing the same in google cloud storage (#961)
    <!-- Add the issue number that is fixed by this PR (In the form Fixes #123) -->
    Fixes #959 
    
    #### Checklist
    
    - [x] I have read the [Contribution & Best practices Guide](https://blog.fossasia.org/open-source-developer-guide-and-best-practices-at-fossasia) and my PR follows them.
    - [x] My branch is up-to-date with the Upstream `development` branch.
    - [x] I have added necessary documentation (if appropriate)
    
    ### Preview Link 
    
    - **Replace XXX with your PR no**
    - Link to live demo: http://pr-XXX-fossasia-badgeyay.surge.sh  
    
    #### Changes proposed in this pull request:
    
    - Endpoint for user profile image update
    - Consuming the already defined methods to upload the profile image to google cloud

---
+++ api/controllers/updateProfile.py
@@ -0,0 +1,56 @@
+import os
+
+from flask import Blueprint, request, jsonify
+from api.utils.errors import ErrorResponse
+from flask import current_app as app
+from api.helpers.uploads import saveToImage
+from api.utils.firebaseUploader import fileUploader
+from api.schemas.errors import (
+    PayloadNotFound,
+    ImageNotFound,
+    UserNotFound,
+    ExtensionNotFound,
+    PNGNotFound
+)
+from api.schemas.user import UpdateUserSchema
+from api.models.user import User
+
+router = Blueprint('updateUserProfile', __name__)
+
+
+@router.route('/profileImage')
+def update_profile_image():
+    try:
+        data = request.get_json()['data']['attributes']
+    except Exception:
+        return ErrorResponse(PayloadNotFound().message, 422, {'Content-Type': 'application/json'}).respond()
+
+    if not data['image']:
+        return ErrorResponse(ImageNotFound().message, 422, {'Content-Type': 'application/json'}).respond()
+
+    if not data['extension']:
+        return ErrorResponse(ExtensionNotFound().message, 422, {'Content-Type': 'application/json'}).respond()
+
+    if data['extension'] != 'png':
+        return ErrorResponse(PNGNotFound().message, 422, {'Content-Type': 'application/json'}).respond()
+
+    image = data['image']
+    extension = data['extension']
+    try:
+        imageName = saveToImage(imageFile=image, extension=extension)
+    except Exception:
+        return ErrorResponse(ImageNotFound().message, 422, {'Content-Type': 'application/json'}).respond()
+
+    uid = data['uid']
+    fetch_user = User.getUser(user_id=uid)
+    if fetch_user is None:
+        return ErrorResponse(UserNotFound(uid).message, 422, {'Content-Type': 'application/json'}).respond()
+    imagePath = os.path.join(app.config.get('BASE_DIR'), 'static', 'uploads', 'image') + '/' + imageName
+    imageLink = fileUploader(imagePath, 'profile/images/' + imageName)
+    fetch_user.photoURL = imageLink
+    fetch_user.save_to_db()
+    try:
+        os.unlink(imagePath)
+    except Exception:
+        print('Unable to delete the temporary file')
+    return jsonify(UpdateUserSchema().dump(fetch_user).data)
