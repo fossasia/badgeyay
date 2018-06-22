bb71076f021d1a881d3ffb0bdc3eb15e8da068ab
Manish Devgan <manish.nsit8@gmail.com>
2018-06-22 21:59:27 +0530

    Adds update profile backend
    Adds update profile backend
    
    Codacy fix
    
    Codacy fix
    
    Codacy fix

--- api/controllers/modifyUser.py
+++ api/controllers/modifyUser.py
@@ -1,11 +1,13 @@
 from flask import Blueprint, jsonify, request
+from api.db import db
 from api.utils.response import Response
+from api.utils.update_user import update_firebase_complete
 from api.models.user import User
 from api.helpers.verifyToken import loginRequired
 from api.utils.errors import ErrorResponse
 from api.helpers.verifyPassword import verifyPassword
 from werkzeug.security import generate_password_hash
-from api.schemas.user import DeleteUserSchema
+from api.schemas.user import DeleteUserSchema, UpdateProfileSchema, UserSchema
 from api.schemas.errors import (
     PayloadNotFound,
     OperationNotFound,
@@ -83,3 +85,26 @@ def delete_user():
     temp_user = user
     user.delete_user()
     return jsonify(DeleteUserSchema().dump(temp_user).data)
+
+
+@loginRequired
+@router.route('/update_profile', methods=['POST'])
+def update_profile():
+    schema = UpdateProfileSchema()
+    input_data = request.get_json()
+    data, err = schema.load(input_data)
+    if err:
+        return jsonify(err)
+    user = User.getUser(user_id=data['uid'])
+    if data['email'] != '':
+        user.email = data['email']
+    if data['password'] != '':
+        user.password = data['password']
+    if data['username'] != '':
+        user.username = data['username']
+
+    db.session.commit()
+
+    ret = update_firebase_complete(data['uid'], data['username'], data['email'], data['password'])
+    print(ret)
+    return jsonify(UserSchema().dump(user).data)
--- api/utils/update_user.py
+++ api/utils/update_user.py
@@ -25,11 +25,11 @@ def update_firebase_emailVerified(uid):
         return False
 
 
-def update_firebase_password(uid, pwd):
+def update_firebase_password(uid, _pwd):
     try:
         auth.update_user(
             uid=uid,
-            password=pwd
+            password=_pwd
         )
         return True
     except Exception as e:
@@ -41,8 +41,35 @@ def update_firebase_username(uid, username):
     try:
         auth.update_user(
             uid=uid,
-            username=username
+            display_name=username
         )
+        return True
+    except Exception as e:
+        print(e)
+        return False
+
+
+def update_firebase_email(uid, email):
+    try:
+        auth.update_user(
+            uid=uid,
+            email=email
+        )
+        return True
+    except Exception as e:
+        print(e)
+        return False
+
+
+def update_firebase_complete(uid, username, email, _pwd):
+    try:
+        if _pwd != '':
+            ret = update_firebase_password(uid, _pwd)
+        if email != '':
+            ret = update_firebase_email(uid, email)
+        if username != '':
+            ret = update_firebase_username(uid, username)
+        print(ret)
         return True
     except Exception as e:
         print(e)
