20203d23326be55fe72b642406d63c297ecf0330
Parth Shandilya <parth1989shandilya@gmail.com>
2018-07-01 02:04:07 +0900

    Added Pagination in My Badges (#1151)

---
+++ frontend/app/controllers/my-badges.js
@@ -0,0 +1,7 @@
+import Controller from '@ember/controller';
+import Ember from 'ember';
+
+const { inject } = Ember;
+
+export default Controller.extend({
+});
--- frontend/app/routes/my-badges.js
+++ frontend/app/routes/my-badges.js
@@ -1,7 +1,19 @@
 import Route from '@ember/routing/route';
 
+import Ember from 'ember';
+
+const { set } = Ember;
+
 export default Route.extend({
-  model() {
-    return this.get('store').findAll('my-badges');
+  beforeModel(transition) {
+    this._super(...arguments);
+  },
+
+  model(params) {
+    let filter = {};
+    this.set('params', params);
+    filter.state = 'all';
+    filter.page = params.page;
+    return this.get('store').query('my-badges', filter);
   }
 });
