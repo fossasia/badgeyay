fdea2329093b993ff2d01d708d381e0e6bfa7075
ParthS007 <parth1989shandilya@gmail.com>
2018-06-30 21:09:32 +0530

    Added Pagination in My Badges

--- frontend/app/components/user-component/my-badges.js
+++ frontend/app/components/user-component/my-badges.js
@@ -1,4 +1,48 @@
 import Component from '@ember/component';
+import Ember from 'ember';
+
+const { inject } = Ember;
 
 export default Component.extend({
+  queryParams : ['page'],
+  page        : 1,
+  notify      : inject.service('notify'),
+  actions     : {
+    nextPage() {
+      let filter = {};
+      if (this.page > 1) {
+        filter.page = this.page + 1;
+        this.get('store').query('my-badges', filter)
+          .then(records => {
+            if (records.length > 0) {
+              this.set('my-badges', records);
+              this.set('page', this.page + 1);
+            } else {
+              this.notify.error('No More Badges found');
+            }
+          })
+          .catch(err => {
+            this.get('notify').error('Please try again!');
+          });
+      } else {
+        this.notify.error('No More Badges Found');
+      }
+    },
+    prevPage() {
+      let filter = {};
+      if (this.page - 1 > 0) {
+        filter.page = this.page - 1;
+        this.get('store').query('my-badges', filter)
+          .then(records => {
+            this.set('my-badges', records);
+            this.set('page', this.page - 1);
+          })
+          .catch(err => {
+            this.get('notify').error('Please try again!');
+          });
+      } else {
+        this.notify.error('No More Badges Found');
+      }
+    }
+  }
 });
