1948bd92fada0702d32cfe2225aa89dc6e305949
Manish Devgan <manish.nsit8@gmail.com>
2018-07-02 18:34:55 +0530

    Blocks login and signup on authentication
    Blocks login and signup on authentication

--- frontend/app/routes/signup.js
+++ frontend/app/routes/signup.js
@@ -3,4 +3,10 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  beforeModel(transition) {
+    var isAuthenticated = this.get('session.content.isAuthenticated');
+    if (isAuthenticated) {
+      this.transitionTo('application');
+    }
+  }
 });
