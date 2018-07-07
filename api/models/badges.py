4b82b6f1491e0fbd9f24d38c2684f96b8f5c483e
Manish Devgan <manish.nsit8@gmail.com>
2018-07-07 22:06:53 +0530

    Modifies the my-badges component

--- api/models/badges.py
+++ api/models/badges.py
@@ -12,6 +12,7 @@ class Badges(db.Model):
     text_color = db.Column(db.String(100), nullable=False)
     badge_size = db.Column(db.String(100), nullable=False)
     download_link = db.Column(db.String)
+    image_link = db.Column(db.String)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     user_id = db.Column(db.String(100), db.ForeignKey('User.id', ondelete='CASCADE'))
 
