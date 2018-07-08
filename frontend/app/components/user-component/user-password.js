663fe5796a790ed87a28453af7e786ca397a7cd8
Parth Shandilya <parth1989shandilya@gmail.com>
2018-07-09 04:52:56 +0900

    Added Validation in User Password Form (#1248)

--- frontend/app/components/user-component/user-password.js
+++ frontend/app/components/user-component/user-password.js
@@ -11,5 +11,38 @@ export default Component.extend({
       let password = this.get('newPassword');
       this.get('sendUserPassword')(password);
     }
+  },
+
+
+  didRender() {
+    this.$('.ui.form')
+      .form({
+        inline : true,
+        delay  : false,
+        fields : {
+          newPassword: {
+            identifier : 'newPassword',
+            rules      : [
+              {
+                type   : 'empty',
+                prompt : 'Please enter a password'
+              },
+              {
+                type   : 'minLength[6]',
+                prompt : 'Your password must have at least {ruleValue} characters'
+              }
+            ]
+          },
+          newPasswordVerify: {
+            identifier : 'newPasswordVerify',
+            rules      : [
+              {
+                type   : 'match[newPassword]',
+                prompt : 'Passwords do not match'
+              }
+            ]
+          }
+        }
+      });
   }
 });
