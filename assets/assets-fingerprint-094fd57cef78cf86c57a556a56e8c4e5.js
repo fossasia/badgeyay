7720e11012c66c40e0197753cdd532535d484162
Travis CI <noreply+travis@fossasia.org>
2018-07-12 17:12:14 +0000

    Travis CI Clean Deploy 0a54d75

---
+++ assets/assets-fingerprint-094fd57cef78cf86c57a556a56e8c4e5.js
@@ -0,0 +1,1 @@
+(function(a){a.ASSET_FINGERPRINT_HASH="-094fd57cef78cf86c57a556a56e8c4e5"})(window);
--- assets/assets-fingerprint-3e0feaf677f9714bbdc8076347591605.js
+++
@@ -1,1 +0,0 @@
-(function(a){a.ASSET_FINGERPRINT_HASH="-3e0feaf677f9714bbdc8076347591605"})(window);
