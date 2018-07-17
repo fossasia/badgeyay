e38f5d4b4f09ea6b9dba681c41f5933bb74a1b6a
Travis CI <noreply+travis@fossasia.org>
2018-07-17 16:48:28 +0000

    Travis CI Clean Deploy 22f5620

--- assets/assets-fingerprint-163f1537cdc0d5df77247e781b2fbc70.js
+++
@@ -1,1 +0,0 @@
-(function(a){a.ASSET_FINGERPRINT_HASH="-163f1537cdc0d5df77247e781b2fbc70"})(window);
---
+++ assets/assets-fingerprint-ca6678c1000314316be906e80cc6c703.js
@@ -0,0 +1,1 @@
+(function(a){a.ASSET_FINGERPRINT_HASH="-ca6678c1000314316be906e80cc6c703"})(window);
