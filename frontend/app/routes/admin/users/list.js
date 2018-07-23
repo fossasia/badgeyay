610fcd11613af053df2ced1d9d4c450f34f8f583
Yash Ladha <yashladhapankajladha123@gmail.com>
2018-07-23 13:38:49 +0530

    Fixes #1441 : Added search functionality in user tab in admin

--- frontend/app/routes/admin/users/list.js
+++ frontend/app/routes/admin/users/list.js
@@ -10,6 +10,7 @@ export default Route.extend({
 
   model(params) {
     let filter = {};
+    this.set('single', false);
     this.set('params', params);
     if (params.users_status === 'all') {
       filter.state = 'all';
@@ -17,6 +18,10 @@ export default Route.extend({
       filter.state = 'active';
     } else if (params.users_status === 'deleted') {
       filter.state = 'deleted';
+    } else {
+      filter.email = params.users_status;
+      this.set('single', true);
+      return this.get('store').queryRecord('all-user', filter);
     }
     filter.page = params.page;
     return this.get('store').query('all-user', filter);
@@ -25,6 +30,10 @@ export default Route.extend({
   setupController(controller, model) {
     this._super(...arguments);
     set(controller, 'state', this.params.users_status);
+    set(controller, 'single', this.single);
+    if (model.length == 0) {
+      set(controller, 'empty', true);
+    }
     set(controller, 'users', model);
     if (model.length < 10) {
       set(controller, 'allow_prev', false);
