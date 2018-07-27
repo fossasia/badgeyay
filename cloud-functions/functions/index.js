2d5290941262ed204ae544b32568a15717daa4b9
Yash Ladha <yashladhapankajladha123@gmail.com>
2018-07-27 16:46:16 +0530

    Fixes #1510 : Modified mail templates in cloud functions

--- cloud-functions/functions/index.js
+++ cloud-functions/functions/index.js
@@ -1,13 +1,13 @@
-'use strict';
+"use strict";
 
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-const firebase = require('firebase');
-const crypto = require('crypto');
-const moment = require('moment');
-var serviceAccount = require('./config/serviceKey.json');
-var clientAccount = require('./config/clientKey.json');
-const fs = require('fs');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const firebase = require("firebase");
+const crypto = require("crypto");
+const moment = require("moment");
+var serviceAccount = require("./config/serviceKey.json");
+var clientAccount = require("./config/clientKey.json");
+const fs = require("fs");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -17,121 +17,123 @@ admin.initializeApp({
 
 firebase.initializeApp(clientAccount);
 
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 
 const gmailEmail = functions.config().gmail.email;
 const gmailPassword = functions.config().gmail.password;
 const mailTransport = nodemailer.createTransport({
-  service: 'gmail',
+  service: "gmail",
   auth: {
     user: gmailEmail,
     pass: gmailPassword
   }
 });
 
-const APP_NAME = 'Badgeyay';
-const BASE_URL = 'http://badgeyay.com/';
-const PASSWORD_RESET_LINK = 'http://badgeyay.com/#/reset/password?token=';
-var password = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa';
+const APP_NAME = "Badgeyay";
+const BASE_URL = "http://badgeyay.com/";
+const PASSWORD_RESET_LINK = "http://badgeyay.com/#/reset/password?token=";
+var password = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
 var db = admin.database();
-var REASONS = ['verificaton', 'greeting', 'passwordReset', 'badgeGeneration'];
+var REASONS = ["verificaton", "greeting", "passwordReset", "badgeGeneration"];
 
+var encrypt = function(input, password, callback) {
+  var m = crypto.createHash("md5");
+  m.update(password);
+  var key = m.digest("hex");
 
-var encrypt = function (input, password, callback) {
-  var m = crypto.createHash('md5');
-  m.update(password)
-  var key = m.digest('hex');
+  m = crypto.createHash("md5");
+  m.update(password + key);
+  var iv = m.digest("hex");
 
-  m = crypto.createHash('md5');
-  m.update(password + key)
-  var iv = m.digest('hex');
+  var data = new Buffer(input, "utf8").toString("binary");
 
-  var data = new Buffer(input, 'utf8').toString('binary');
-
-  var cipher = crypto.createCipheriv('aes-256-cbc', key, iv.slice(0, 16));
+  var cipher = crypto.createCipheriv("aes-256-cbc", key, iv.slice(0, 16));
 
   // UPDATE: crypto changed in v0.10
   // https://github.com/joyent/node/wiki/Api-changes-between-v0.8-and-v0.10
   var nodev = process.version.match(/^v(\d+)\.(\d+)/);
   var encrypted;
 
-  if (nodev[1] === '0' && parseInt(nodev[2]) < 10) {
-    encrypted = cipher.update(data, 'binary') + cipher.final('binary');
+  if (nodev[1] === "0" && parseInt(nodev[2]) < 10) {
+    encrypted = cipher.update(data, "binary") + cipher.final("binary");
   } else {
-    encrypted = cipher.update(data, 'utf8', 'binary') + cipher.final('binary');
+    encrypted = cipher.update(data, "utf8", "binary") + cipher.final("binary");
   }
 
-  var encoded = new Buffer(encrypted, 'binary').toString('base64');
+  var encoded = new Buffer(encrypted, "binary").toString("base64");
 
   callback(encoded);
 };
 
-var decrypt = function (input, password, callback) {
+var decrypt = function(input, password, callback) {
   // Convert urlsafe base64 to normal base64
-  input = input.replace(/-/g, '+').replace(/_/g, '/');
+  input = input.replace(/-/g, "+").replace(/_/g, "/");
   // Convert from base64 to binary string
-  var edata = new Buffer(input, 'base64').toString('binary')
+  var edata = new Buffer(input, "base64").toString("binary");
 
   // Create key from password
-  var m = crypto.createHash('md5');
-  m.update(password)
-  var key = m.digest('hex');
+  var m = crypto.createHash("md5");
+  m.update(password);
+  var key = m.digest("hex");
 
   // Create iv from password and key
-  m = crypto.createHash('md5');
-  m.update(password + key)
-  var iv = m.digest('hex');
+  m = crypto.createHash("md5");
+  m.update(password + key);
+  var iv = m.digest("hex");
 
   // Decipher encrypted data
-  var decipher = crypto.createDecipheriv('aes-256-cbc', key, iv.slice(0, 16));
+  var decipher = crypto.createDecipheriv("aes-256-cbc", key, iv.slice(0, 16));
 
   // UPDATE: crypto changed in v0.10
   // https://github.com/joyent/node/wiki/Api-changes-between-v0.8-and-v0.10
   var nodev = process.version.match(/^v(\d+)\.(\d+)/);
   var decrypted, plaintext;
 
-  if (nodev[1] === '0' && parseInt(nodev[2]) < 10) {
-    decrypted = decipher.update(edata, 'binary') + decipher.final('binary');
-    plaintext = new Buffer(decrypted, 'binary').toString('utf8');
+  if (nodev[1] === "0" && parseInt(nodev[2]) < 10) {
+    decrypted = decipher.update(edata, "binary") + decipher.final("binary");
+    plaintext = new Buffer(decrypted, "binary").toString("utf8");
   } else {
-    plaintext = (decipher.update(edata, 'binary', 'utf8') + decipher.final('utf8'));
+    plaintext =
+      decipher.update(edata, "binary", "utf8") + decipher.final("utf8");
   }
 
   callback(plaintext);
 };
 
-
-String.prototype.format = function () {
+String.prototype.format = function() {
   var formatted = this;
   for (var prop in arguments[0]) {
-    var regexp = new RegExp('\\{' + prop + '\\}', 'gi');
+    var regexp = new RegExp("\\{" + prop + "\\}", "gi");
     formatted = formatted.replace(regexp, arguments[0][prop]);
   }
   return formatted;
 };
 
-exports.sendVerificationMail = functions.auth.user().onCreate((user) => {
+exports.sendVerificationMail = functions.auth.user().onCreate(user => {
   const uid = user.uid;
   if (user.emailVerified) {
-    console.log('User has email already verified: ', user.email);
+    console.log("User has email already verified: ", user.email);
     sendGreetingMail(user.email, user.displayName);
     return 0;
   } else {
     let userEmail = user.email;
     const mailOptions = {
       from: `${APP_NAME}<noreply@firebase.com>`,
-      to: userEmail,
+      to: userEmail
     };
 
     encrypt(userEmail, password, encoded => {
       var resp = {
-        link: BASE_URL + 'verify/email?id=' + encodeURIComponent(encoded)
-      }
-      mailOptions.subject = 'Please verify your Email | Badgeyay';
-      mailOptions.html = '<p>Please verify your email ID by clicking on this <a href={link}>Link</a></p>'.format(resp);
-      return mailTransport.sendMail(mailOptions)
+        link: BASE_URL + "verify/email?id=" + encodeURIComponent(encoded)
+      };
+      mailOptions.subject = "Please verify your Email | Badgeyay";
+      mailOptions.html = "<p>Please verify your email ID by clicking on this <a href={link}>Link</a></p>".format(
+        resp
+      );
+      return mailTransport
+        .sendMail(mailOptions)
         .then(() => {
-          console.log('Verification Mail Sent');
+          console.log("Verification Mail Sent");
           writeMailData(uid, "success", 0);
           return 0;
         })
@@ -144,101 +146,131 @@ exports.sendVerificationMail = functions.auth.user().onCreate((user) => {
   }
 });
 
-exports.sendBadgeMail = functions.database.ref('/badgeMails/{userId}/{date}')
+exports.sendBadgeMail = functions.database
+  .ref("/badgeMails/{userId}/{date}")
   .onCreate((snapshot, context) => {
     const payload = snapshot.val();
     const uid = context.params.userId;
-    return admin.auth().getUser(uid)
+    return admin
+      .auth()
+      .getUser(uid)
       .then(record => {
-        return sendBadgeGenMail(uid, record.email, record.displayName, payload['badgeId'], payload['badgeLink']);
+        var resp = {
+          badgeId: payload["badgeId"],
+          badgeLink: payload["badgeLink"],
+          displayName: record.displayName ? record.displayName : ""
+        };
+        return sendBadgeGenMail(uid, record.email, resp);
       })
-      .catch(() => { return -1 });
+      .catch(() => {
+        return -1;
+      });
   });
 
-function sendBadgeGenMail(uid, email, displayName, badgeId, badgeLink) {
+function sendBadgeGenMail(uid, email, resp) {
   const mailOptions = {
     from: `${APP_NAME}<noreply@firebase.com>`,
-    to: email,
+    to: email
   };
 
-  mailOptions.subject = `Badge Generated ${badgeId}`;
-  mailOptions.html = `<p> Hello ${displayName || ''}! Your badge is generated successfully, please visit the <a href=${badgeLink}>link</a> to download badge</p>`;
-  return mailTransport.sendMail(mailOptions).then(() => {
-    writeMailData(uid, "success", 3);
-    return console.log('Badge mail sent to: ', email)
-  }).catch((err) => {
-    console.error(err.message);
-    return -1;
-  });
+  mailOptions.subject = "Badge Generated {badgeId}".format(resp);
+  mailOptions.html = "<p> Hello {displayName}! Your badge is generated successfully, please visit the <a href={badgeLink}>link</a> to download badge</p>".format(
+    resp
+  );
+  return mailTransport
+    .sendMail(mailOptions)
+    .then(() => {
+      writeMailData(uid, "success", 3);
+      return console.log("Badge mail sent to: ", email);
+    })
+    .catch(err => {
+      console.error(err.message);
+      return -1;
+    });
 }
-
 
 function writeMailData(uid, state, reason) {
-  if (state === 'success') {
-    db.ref('mails')
+  if (state === "success") {
+    db.ref("mails")
       .push()
-      .set({
-        uid: uid,
-        state: state,
-        reason: reason,
-        date: moment.utc().format()
-      }, err => {
-        if (err) {
-          console.log('Unable to save to database');
-        } else {
-          console.log('Mail data saved successfully');
+      .set(
+        {
+          uid: uid,
+          state: state,
+          reason: reason,
+          date: moment.utc().format()
+        },
+        err => {
+          if (err) {
+            console.log("Unable to save to database");
+          } else {
+            console.log("Mail data saved successfully");
+          }
         }
-      });
+      );
   }
-  if (state === 'error') {
-    console.log('Unable to send Mail');
+  if (state === "error") {
+    console.log("Unable to send Mail");
   }
 }
 
-function sendGreetingMail(uid, email, displayName) {
+function sendGreetingMail(uid, email, resp) {
   const mailOptions = {
     from: `${APP_NAME}<noreply@firebase.com>`,
-    to: email,
+    to: email
   };
 
   mailOptions.subject = `Welcome to Badgeyay`;
-  mailOptions.text = `Hey ${displayName || ''}! Welcome to Badgeyay. We welcome you onboard and pleased to offer you service.`;
-  return mailTransport.sendMail(mailOptions).then(() => {
-    writeMailData(uid, "success", 1);
-    return console.log('Welcome mail sent to: ', email)
-  }).catch((err) => {
-    console.error(err.message);
-    return -1;
-  });
+  mailOptions.text = "Hey {displayName}! Welcome to Badgeyay. We welcome you onboard and pleased to offer you service.".format(
+    resp
+  );
+  return mailTransport
+    .sendMail(mailOptions)
+    .then(() => {
+      writeMailData(uid, "success", 1);
+      return console.log("Welcome mail sent to: ", email);
+    })
+    .catch(err => {
+      console.error(err.message);
+      return -1;
+    });
 }
 
 exports.sendWelcomeMail = functions.https.onRequest((req, res) => {
-  let uid = req.query['id'];
-  admin.auth().getUser(uid)
+  let uid = req.query["id"];
+  admin
+    .auth()
+    .getUser(uid)
     .then(userRecord => {
       let email = userRecord.email;
-      let displayName = userRecord.displayName;
-      return sendGreetingMail(uid, email, displayName);
+      var resp = {
+        displayName: userRecord.displayName ? userRecord.displayName : ""
+      };
+      return sendGreetingMail(uid, email, resp);
     })
     .catch(err => {
       console.log(err);
       return -1;
-    })
+    });
 });
 
 exports.sendResetMail = functions.https.onRequest((req, res) => {
-  let token = req.query['token'];
-  let email = req.query['email'];
-  res.setHeader('Content-Type', 'application/json');
+  let token = req.query["token"];
+  let email = req.query["email"];
+  res.setHeader("Content-Type", "application/json");
   sendResetMail(token, email)
     .then(() => {
-      console.log('Reset mail sent to', email);
-      res.json({ data: { attributes: { status: 200 }, id: token, type: 'reset-mails' } });
+      console.log("Reset mail sent to", email);
+      res.json({
+        data: { attributes: { status: 200 }, id: token, type: "reset-mails" }
+      });
       return 0;
     })
     .catch(err => {
       console.error(err);
-      res.json({ data: { attributes: { status: 500 }, id: token, type: 'reset-mails' } });
+      res.json({
+        data: { attributes: { status: 500 }, id: token, type: "reset-mails" }
+      });
       return -1;
     });
 });
@@ -246,11 +278,18 @@ exports.sendResetMail = functions.https.onRequest((req, res) => {
 function sendResetMail(token, email) {
   const mailOptions = {
     from: `${APP_NAME}<noreply@firebase.com>`,
-    to: email,
+    to: email
+  };
+
+  var resp = {
+    email: email,
+    token: token
   };
 
   mailOptions.subject = `Password reset link`;
-  mailOptions.html = '<p>Hey ' + email + '! Here is your password reset <a href=\'' + PASSWORD_RESET_LINK
-    + token + '\'>Link</a> and it will expire in 24 hours.<p>';
+  mailOptions.html =
+    "<p>Hey {email}! Here is your password reset <a href=" +
+    PASSWORD_RESET_LINK +
+    "{token}>Link</a> and it will expire in 24 hours.<p>".format(resp);
   return mailTransport.sendMail(mailOptions);
 }
