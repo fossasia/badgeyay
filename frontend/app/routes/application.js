dce9b46d9f686afa9063b5aae7f351b95b25d807
Yash Ladha <yashladhapankajladha123@gmail.com>
2018-08-03 20:19:36 +0530

    Fixes #1583 : Modified the last login logic to get the Last Login Date and IP

--- frontend/app/routes/application.js
+++ frontend/app/routes/application.js
@@ -1,12 +1,17 @@
 import Route from '@ember/routing/route';
 import Ember from 'ember';
+import ENV from '../config/environment';
+
+const { APP } = ENV;
 
 const { inject } = Ember;
 
 export default Route.extend({
   authToken: inject.service('auth-session'),
   beforeModel() {
-    return this.get('session').fetch().catch(function() {});
+    return this.get('session')
+      .fetch()
+      .catch(function() {});
   },
   async model() {
     const userObj = this.get('session.currentUser');
@@ -16,6 +21,10 @@ export default Route.extend({
       if (adminStatus === 'true') {
         this.authToken.enableAdmin();
       }
+
+      // Update the last login details
+      fetch(APP.backLink + '/user/updateLastLogin/' + uid)
+        .catch(err => console.log(err));
 
       const emailStatus = localStorage.getItem('emailVerified');
       if (emailStatus === 'true') {
@@ -27,13 +36,15 @@ export default Route.extend({
       if (loginToken && loginToken !== undefined) {
         // Persist the login token in the cache
         this.get('store').pushPayload({
-          data: [{
-            id         : loginToken.id,
-            type       : 'login-token',
-            attributes : {
-              token: loginToken.token
+          data: [
+            {
+              id         : loginToken.id,
+              type       : 'login-token',
+              attributes : {
+                token: loginToken.token
+              }
             }
-          }]
+          ]
         });
 
         this.authToken.updateToken(loginToken.token);
@@ -42,40 +53,43 @@ export default Route.extend({
       const permission = JSON.parse(localStorage.getItem('permissions'));
 
       if (permission && permission !== undefined) {
-
         // Sets the permission in service
         this.authToken.setPermission(permission);
 
         // Persist the permissions in the cache
         this.get('store').pushPayload({
-          data: [{
-            id         : permission.id,
-            type       : 'permission',
-            attributes : {
-              isUser  : permission.isUser,
-              isAdmin : permission.isAdmin,
-              isSales : permission.isSales
+          data: [
+            {
+              id         : permission.id,
+              type       : 'permission',
+              attributes : {
+                isUser  : permission.isUser,
+                isAdmin : permission.isAdmin,
+                isSales : permission.isSales
+              }
             }
-          }]
+          ]
         });
       }
 
       this.get('store').pushPayload({
-        data: [{
-          id         : uid,
-          type       : 'user',
-          attributes : {
-            uid,
-            'username' : userObj.displayName,
-            'email'    : userObj.email,
-            'photoURL' : userObj.photoURL
-          },
-          relationships: {}
-        }]
+        data: [
+          {
+            id         : uid,
+            type       : 'user',
+            attributes : {
+              uid,
+              username : userObj.displayName,
+              email    : userObj.email,
+              photoURL : userObj.photoURL
+            },
+            relationships: {}
+          }
+        ]
       });
     }
     return {
-      'socialMedia': await this.get('store').findAll('social-content')
+      socialMedia: await this.get('store').findAll('social-content')
     };
   }
 });
