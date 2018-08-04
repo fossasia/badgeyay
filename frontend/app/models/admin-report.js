44b14441dad79a1925cf009b45ab66aab7da41e2
Yash Ladha <yashladhapankajladha123@gmail.com>
2018-08-04 23:18:05 +0530

    Fixes #1607 : Added badge deletion count in reports

--- frontend/app/models/admin-report.js
+++ frontend/app/models/admin-report.js
@@ -3,8 +3,9 @@ import DS from 'ember-data';
 const { Model, attr } = DS;
 
 export default Model.extend({
-  mailSentCount     : attr('string'),
-  badgeCount        : attr('string'),
-  userCreationCount : attr('string'),
-  userDeletionCount : attr('string')
+  mailSentCount      : attr('string'),
+  badgeCount         : attr('string'),
+  userCreationCount  : attr('string'),
+  userDeletionCount  : attr('string'),
+  badgeDeletionCount : attr('string')
 });
