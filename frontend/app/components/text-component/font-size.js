37b4bd3af79411fe7b7debe3864c409f430bd1b2
Manish Devgan <manish.nsit8@gmail.com>
2018-08-06 22:03:04 +0530

    Adds apply-to-all checkbox (#1622)

--- frontend/app/components/text-component/font-size.js
+++ frontend/app/components/text-component/font-size.js
@@ -14,10 +14,28 @@ export default Component.extend({
     this.fonttype3 = '';
     this.fonttype4 = '';
     this.fonttype5 = '';
+    this.fonttypeall = false;
+    this.fontsizeall = false;
     this._super(...arguments);
   },
 
   click() {
+    if (this.fonttypeall) {
+      var temp_type = this.fonttype1 || this.fonttype2 || this.fonttype3 || this.fonttype4 || this.fonttype5;
+      this.set('fonttype1', temp_type);
+      this.set('fonttype2', temp_type);
+      this.set('fonttype3', temp_type);
+      this.set('fonttype4', temp_type);
+      this.set('fonttype5', temp_type);
+    }
+    if (this.fontsizeall) {
+      var temp_font = this.font1 || this.font2 || this.font3 || this.font4 || this.font5;
+      this.set('font1', temp_font);
+      this.set('font2', temp_font);
+      this.set('font3', temp_font);
+      this.set('font4', temp_font);
+      this.set('font5', temp_font);
+    }
     this.get('sendDefSize')([
       this.font1,
       this.font2,
