0c4113f84f6407003584d4b4f2d0cd361337f635
sk9331657 <sk9331657@gmail.com>
2018-09-18 14:23:21 +0530

    Solve_1630

--- frontend/app/components/text-component/font-size.js
+++ frontend/app/components/text-component/font-size.js
@@ -20,21 +20,45 @@ export default Component.extend({
   },
 
   click() {
-    if (this.fonttypeall) {
+    if (document.getElementsByName("fonttypeall")[0].checked === true) {
+      console.log("inside above");
       var temp_type = this.fonttype1 || this.fonttype2 || this.fonttype3 || this.fonttype4 || this.fonttype5;
       this.set('fonttype1', temp_type);
       this.set('fonttype2', temp_type);
       this.set('fonttype3', temp_type);
       this.set('fonttype4', temp_type);
       this.set('fonttype5', temp_type);
+      var r = document.getElementsByClassName('selection');
+      for (var i = 1; i < r.length; i = i + 2) {
+        document.getElementById(r[i].id).style = 'pointer-events:none';
+      }
     }
-    if (this.fontsizeall) {
+    else if(document.getElementsByName("fonttypeall")[0].checked === false)
+    {
+      var r = document.getElementsByClassName('selection');
+      for (var i = 1; i < r.length; i = i + 2) {
+        document.getElementById(r[i].id).style = 'pointer-events:auto';
+      }
+    }
+    if (document.getElementsByName("fontsizeall")[0].checked === true) {
       var temp_font = this.font1 || this.font2 || this.font3 || this.font4 || this.font5;
       this.set('font1', temp_font);
       this.set('font2', temp_font);
       this.set('font3', temp_font);
       this.set('font4', temp_font);
       this.set('font5', temp_font);
+      var r = document.getElementsByClassName('selection');
+      for (var i = 2; i < r.length; i = i + 2) {
+        document.getElementById(r[i].id).style = 'pointer-events:none';
+      }
+    }
+    else if(document.getElementsByName("fontsizeall")[0].checked === false)
+    {
+      var r = document.getElementsByClassName('selection');
+      for (var i = 2; i < r.length; i = i + 2) {
+        document.getElementById(r[i].id).style = 'pointer-events:auto';
+      }
+
     }
     this.get('sendDefSize')([
       this.font1,
