1b9992a8bfa715102f7f43e746295cb42f581258
sk9331657 <sk9331657@gmail.com>
2018-09-24 22:53:20 +0530

    "SolvingPreviewFinal"

--- frontend/app/components/create-badge.js
+++ frontend/app/components/create-badge.js
@@ -1,37 +1,21 @@
 import Component from '@ember/component';
 import Ember from 'ember';
-
+const EmberObject  = Ember;
 
 export default Component.extend({
   didInsertElement() {
-    Ember.$(window).on('resize scroll', function (e) {
-      if (e.type === "scroll" && this.scrollY >= 137 && this.window.innerWidth >= 764 && this.scrollY <1278) {
-        console.log("1");
-
-        this.document.getElementsByClassName('six wide column preview')[0].style.position = 'fixed';
-        this.document.getElementsByClassName('six wide column preview')[0].style.right = '0';
-        this.document.getElementsByClassName('six wide column preview')[0].style.top = '0';
-
-      }
-      else if (e.type === "scroll" && this.window.innerWidth >= 764 && this.scrollY >=1278) {
-        this.console.log("2");
-        this.document.getElementsByClassName('six wide column preview')[0].style.position = 'fixed';
-        this.document.getElementsByClassName('bgImg ui raised segment')[0].style.height = '570px';
-
-        // this.document.getElementsByClassName('six wide column preview')[0].style.right = '0';
-        // this.document.getElementsByClassName('six wide column preview')[0].style.bottom = '0px';
-
-      }
-      else {
-        this.console.log("3");
-        this.document.getElementsByClassName('bgImg ui raised segment')[0].style.height = '600px';
-
-        this.document.getElementsByClassName('six wide column preview')[0].style.position = 'inherit';
-
+    EmberObject.$(window).on('resize scroll', function(e) {
+      if (e.type === 'scroll' && this.scrollY >= 137 && this.window.innerWidth >= 764 && this.scrollY < 1278) {
+        EmberObject.$(document)[0].getElementsByClassName('six wide column preview')[0].style.position = 'fixed';
+        EmberObject.$(document)[0].getElementsByClassName('six wide column preview')[0].style.right = '0';
+        EmberObject.$(document)[0].getElementsByClassName('six wide column preview')[0].style.top = '0';
+      } else if (e.type === 'scroll' && this.window.innerWidth >= 764 && this.scrollY >= 1278) {
+        EmberObject.$(document)[0].getElementsByClassName('six wide column preview')[0].style.position = 'fixed';
+        EmberObject.$(document)[0].getElementsByClassName('bgImg ui raised segment')[0].style.height = '570px';
+      } else {
+        EmberObject.$(document)[0].getElementsByClassName('bgImg ui raised segment')[0].style.height = '600px';
+        EmberObject.$(document)[0].getElementsByClassName('six wide column preview')[0].style.position = 'inherit';
       }
     });
-       
-
-
   }
 });
