b77dd7adedfc5f1b8983ef90d252ea0858c6ea71
sk9331657 <sk9331657@gmail.com>
2018-09-25 14:53:45 +0530

    "Solves 1719"

--- frontend/app/routes/create-badges.js
+++ frontend/app/routes/create-badges.js
@@ -4,12 +4,19 @@ import Route from '@ember/routing/route';
 const { RSVP, set } = Ember;
 
 export default Route.extend({
+  beforeModel() {
+    if (this.get('session.currentUser') == undefined) {
+      this.transitionTo('login');
+    }
+  }
+  ,
   model() {
     return RSVP.hash({
-      def_images : this.get('store').findAll('def-image'),
-      user       : this.get('store').findRecord('user', this.get('session.currentUser').uid)
+      def_images: this.get('store').findAll('def-image'),
+      user: this.get('store').findRecord('user', this.get('session.currentUser').uid)
     });
   },
+
 
   setupController(controller, model) {
     this._super(...arguments);
