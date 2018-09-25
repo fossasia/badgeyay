3c29f4174fd8c35f7c09e500a7ee1e8b9ae9fdac
sk9331657 <sk9331657@gmail.com>
2018-09-25 13:54:22 +0530

    "Final Commit"

--- frontend/app/components/text-component/font-size.js
+++ frontend/app/components/text-component/font-size.js
@@ -1,7 +1,10 @@
 import Component from '@ember/component';
+import Ember from 'ember';
 
 export default Component.extend({
+
   init() {
+    this.EmberObject = Ember;
     this.defFontSize = '';
     this.textZoom = false;
     this.font1 = '';
@@ -20,42 +23,40 @@ export default Component.extend({
   },
 
   click() {
-    if (document.getElementsByName('fonttypeall')[0].checked === true) {
+    if (this.EmberObject.$('[name="fonttypeall"]')[0].checked === true) {
       var temp_type = this.fonttype1 || this.fonttype2 || this.fonttype3 || this.fonttype4 || this.fonttype5;
       this.set('fonttype1', temp_type);
       this.set('fonttype2', temp_type);
       this.set('fonttype3', temp_type);
       this.set('fonttype4', temp_type);
       this.set('fonttype5', temp_type);
-      let sel1 = document.getElementsByClassName('selection');
+      let sel1 = this.EmberObject.$('.selection');
       for (let i = 1; i < sel1.length; i = i + 2) {
-        document.getElementById(sel1[i].id).style = 'pointer-events:none';
+        this.EmberObject.$('#' + sel1[i].id)[0].style = 'pointer-events:none';
       }
     }
-    if (document.getElementsByName('fonttypeall')[0].checked === false) {
-      let sel1 = document.getElementsByClassName('selection');
+    if (this.EmberObject.$('[name="fonttypeall"]')[0].checked === false) {
+      let sel1 = this.EmberObject.$('.selection');
       for (let j = 1; j < sel1.length; j = j + 2) {
-        document.getElementById(sel1[j].id).style = 'pointer-events:auto';
+        this.EmberObject.$('#' + sel1[j].id)[0].style = 'pointer-events:auto';
       }
     }
-    if (document.getElementsByName('fontsizeall')[0].checked === true) {
+    if (this.EmberObject.$('[name="fontsizeall"]')[0].checked === true) {
       var temp_font = this.font1 || this.font2 || this.font3 || this.font4 || this.font5;
       this.set('font1', temp_font);
       this.set('font2', temp_font);
       this.set('font3', temp_font);
       this.set('font4', temp_font);
       this.set('font5', temp_font);
-      let r = document.getElementsByClassName('selection');
+      let r = this.EmberObject.$('.selection');
       for (let i = 2; i < r.length; i = i + 2) {
-        document.getElementById(r[i].id).style = 'pointer-events:none';
-
+        this.EmberObject.$('#' + r[i].id)[0].style = 'pointer-events:none';
       }
-
     }
-    if (document.getElementsByName('fontsizeall')[0].checked === false) {
-      let r = document.getElementsByClassName('selection');
+    if (this.EmberObject.$('[name="fonttypeall"]')[0].checked === false) {
+      let r = this.EmberObject.$('.selection');
       for (let i = 2; i < r.length; i = i + 2) {
-        document.getElementById(r[i].id).style = 'pointer-events:auto';
+        this.EmberObject.$('#' + r[i].id)[0].style = 'pointer-events:auto';
       }
     }
     this.get('sendDefSize')([
