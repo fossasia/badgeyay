79a0bedb4b19a8c27fe3100b6b06681971b57af9
SHUBHAM KUMAR <sk9331657@gmail.com>
2018-09-25 21:48:41 +0530

    Update create-badges.js

--- frontend/app/routes/create-badges.js
+++ frontend/app/routes/create-badges.js
@@ -10,13 +10,8 @@ export default Route.extend({
     }
   },
   model() {
-    return RSVP.hash({
-      def_images : this.get('store').findAll('def-image'),
-      user : this.get('store').findRecord('user', this.get('session.currentUser').uid)
-    });
+    return RSVP.hash({ 'def_images': this.get('store').findAll('def-image'), 'user': this.get('store').findRecord('user', this.get('session.currentUser').uid) });
   },
-
-
   setupController(controller, model) {
     this._super(...arguments);
     set(controller, 'defImages', model.def_images);
