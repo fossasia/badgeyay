377e0219f509ac2526200a3572cbc66b7eb7ae3c
SHUBHAM KUMAR <sk9331657@gmail.com>
2018-09-26 18:46:32 +0530

    Update create-badges.js

--- frontend/app/routes/create-badges.js
+++ frontend/app/routes/create-badges.js
@@ -5,16 +5,16 @@ const { RSVP, set } = Ember;
 
 export default Route.extend({
   beforeModel() {
-    if (this.get('session.currentUser') === undefined) {
+    if (this.get('session.currentUser') == undefined) {
       this.transitionTo('login');
-    },
-    model() {
+    }
+  },
+  model() {
     return RSVP.hash({
       def_images : this.get('store').findAll('def-image'),
       user       : this.get('store').findRecord('user', this.get('session.currentUser').uid)
     });
   },
-  
   setupController(controller, model) {
     this._super(...arguments);
     set(controller, 'defImages', model.def_images);
