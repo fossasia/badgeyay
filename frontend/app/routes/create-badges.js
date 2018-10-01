e7fdaf8523a6f8e617adbe628ddbb16b3bef89ae
SHUBHAM KUMAR <sk9331657@gmail.com>
2018-10-02 04:39:21 +0530

    Fixes #1719 : Added Before Model for redirecting to home page (#1732)
    * accepting only csv files
    
    * "Improving Toggling Button "
    
    * Update create-badge.scss
    
    * Update create-badges.hbs
    
    * Update create-badge.scss
    
    "Solves 1719"
    
    "FINAL CHANGE"
    
    Update create-badges.js
    
    "Added Routes Config got My-badges And Settigns Route"

--- frontend/app/routes/create-badges.js
+++ frontend/app/routes/create-badges.js
@@ -4,13 +4,17 @@ import Route from '@ember/routing/route';
 const { RSVP, set } = Ember;
 
 export default Route.extend({
+  beforeModel() {
+    if (this.get('session.currentUser') === undefined) {
+      this.transitionTo('login');
+    }
+  },
   model() {
     return RSVP.hash({
       def_images : this.get('store').findAll('def-image'),
       user       : this.get('store').findRecord('user', this.get('session.currentUser').uid)
     });
   },
-
   setupController(controller, model) {
     this._super(...arguments);
     set(controller, 'defImages', model.def_images);
--- frontend/app/routes/settings.js
+++ frontend/app/routes/settings.js
@@ -4,6 +4,11 @@ import Route from '@ember/routing/route';
 const { RSVP, set } = Ember;
 
 export default Route.extend({
+  beforeModel() {
+    if (this.get('session.uid') === undefined) {
+      this.transitionTo('login');
+    }
+  },
   model() {
     return RSVP.hash({
       user: this.get('store').peekAll('user').slice(0, 1)[0]
