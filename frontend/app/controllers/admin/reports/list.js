fb72491537b47ec3801bcf1b5c4651f122cfa7ec
SHUBHAM KUMAR <sk9331657@gmail.com>
2018-10-06 10:01:02 +0530

    Fix - 1602 - Indicate the Total no of Pages in Pagination (#1751)

--- frontend/app/controllers/admin/reports/list.js
+++ frontend/app/controllers/admin/reports/list.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 import Controller from '@ember/controller';
-
+const EmberObject = Ember;
 const { inject } = Ember;
 
 export default Controller.extend({
@@ -10,6 +10,8 @@ export default Controller.extend({
   allow_next    : true,
   allow_prev    : false,
   allow         : true,
+  allowprevpage : false,
+  arrayno       : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
   actions       : {
     nextPage() {
       let filters = {};
@@ -62,6 +64,67 @@ export default Controller.extend({
             });
           });
       }
+    },
+    getPage(input) {
+      let filters = {};
+      filters.page = input;
+      this.get('store').query('admin-report', filters)
+        .then(record => {
+          console.log(this.page);
+          this.set('reports', record);
+          this.set('page', input);
+        })
+        .catch(() => {
+          this.get('notifications').clearAll();
+          this.get('notifications').error('Unable to process request', {
+            autoClear     : true,
+            clearDuration : 1500
+          });
+        });
+    },
+    nextpages() {
+      for (var r = 0; r < 10; r++) {
+        EmberObject.set(this.arrayno, r, this.arrayno[r] + 10);
+      }
+      if (this.arrayno[0] > 1) {
+        this.set('allowprevpage', true);
+      }
+      let filters = {};
+      filters.page = this.arrayno[0];
+      this.get('store').query('admin-report', filters)
+        .then(record => {
+          this.set('reports', record);
+          this.set('page', this.arrayno[0]);
+        })
+        .catch(() => {
+          this.get('notifications').clearAll();
+          this.get('notifications').error('Unable to process request', {
+            autoClear     : true,
+            clearDuration : 1500
+          });
+        });
+    },
+    prevpages() {
+      for (var r = 0; r < 10; r++) {
+        EmberObject.set(this.arrayno, r, this.arrayno[r] - 10);
+      }
+      if (this.arrayno[0] <= 1) {
+        this.set('allowprevpage', false);
+      }
+      let filters = {};
+      filters.page = this.arrayno[0];
+      this.get('store').query('admin-report', filters)
+        .then(record => {
+          this.set('reports', record);
+          this.set('page', this.arrayno[0]);
+        })
+        .catch(() => {
+          this.get('notifications').clearAll();
+          this.get('notifications').error('Unable to process request', {
+            autoClear     : true,
+            clearDuration : 1500
+          });
+        });
     }
   }
 });
