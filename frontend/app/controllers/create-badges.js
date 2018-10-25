95a479168baff838e17a36bc1fd9bffb870c12c6
rajvaibhavdubey <17dec004@lnmiit.ac.in>
2018-10-26 00:16:52 +0530

    Custom Image preview now working
    The custom image preview not working bug has now been fixed.
    
    Fixes #1772

--- frontend/app/controllers/create-badges.js
+++ frontend/app/controllers/create-badges.js
@@ -53,6 +53,7 @@ export default Controller.extend({
   socialHandle   : '@dompiero07',
   designation    : 'Social Media Manager',
   prevImageData  : 'https://raw.githubusercontent.com/fossasia/badgeyay/development/frontend/public/images/badge_backgrounds/red_futuristic.png',
+  imageData      : 'https://raw.githubusercontent.com/fossasia/badgeyay/development/frontend/public/images/badge_backgrounds/red_futuristic.png',
   csvClicked() {
     this.set('csvEnable', true);
     this.set('manualEnable', false);
