7ad1723ca17ec9dc829f2d96667429cb945618ae
SHUBHAM KUMAR <sk9331657@gmail.com>
2019-01-15 23:16:22 +0530

    Fix Preview (#1884)

--- frontend/app/components/create-badge.js
+++ frontend/app/components/create-badge.js
@@ -10,13 +10,14 @@ export default Component.extend({
     EmberObject.$(window).bind('resize scroll', function(e) {
       var card = EmberObject.$(document)[0].getElementsByClassName('six wide column preview');
       if (card.length != 0) {
-        if (e.type === 'scroll' && this.scrollY >= 137 && this.window.innerWidth >= 764 && this.scrollY < 1278) {
+        if (e.type === 'scroll' && this.scrollY >= 137 && this.window.innerWidth >= 764 && !(EmberObject.$(window).scrollTop() + EmberObject.$(window).height() > (EmberObject.$(document).height() - 100))) {
           card[0].style.position = 'fixed';
           card[0].style.right = '0';
           card[0].style.top = '0';
-        } else if (e.type === 'scroll' && this.window.innerWidth >= 764 && this.scrollY >= 1278) {
+          EmberObject.$(document)[0].getElementsByClassName('bgImg ui raised segment')[0].style.height = '600px';
+        } else if (e.type === 'scroll' && this.window.innerWidth >= 764 && (EmberObject.$(window).scrollTop() + EmberObject.$(window).height() > (EmberObject.$(document).height() - 100))) {
           card[0].style.position = 'fixed';
-          EmberObject.$(document)[0].getElementsByClassName('bgImg ui raised segment')[0].style.height = '100%';
+          EmberObject.$(document)[0].getElementsByClassName('bgImg ui raised segment')[0].style.height = '530px';
         } else {
           EmberObject.$(document)[0].getElementsByClassName('bgImg ui raised segment')[0].style.height = '100%';
           card[0].style.position = 'inherit';
