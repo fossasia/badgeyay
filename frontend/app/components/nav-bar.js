3212863dd4dfe6f9bbff9bf096ab3ce13b6ee16d
Raj Vaibhav Dubey <33062425+rajvaibhavdubey@users.noreply.github.com>
2019-01-15 21:21:48 +0530

    Without login , Create badges displays error messages (#1936)
    Without login create badges links now displays error messages `Please Login to continue`.
    
    Fixes #1934

--- frontend/app/components/nav-bar.js
+++ frontend/app/components/nav-bar.js
@@ -1,6 +1,10 @@
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
+
 
 export default Component.extend({
+  notifications: service('notification-messages'),
+
   init() {
     this._super(...arguments);
   },
@@ -8,6 +12,13 @@ export default Component.extend({
   actions: {
     logOut() {
       this.get('logOut')();
+    },
+    loginToContinue() {
+      this.get('notifications').clearAll();
+      this.get('notifications').error('Please Login to continue', {
+        autoClear     : true,
+        clearDuration : 1500
+      });
     }
   }
 });
