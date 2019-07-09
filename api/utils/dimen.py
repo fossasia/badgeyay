0e346857cdc35e825a43d437e7396ea0d481b9bc
Abishek V Ashok <abishekvashok@gmail.com>
2019-07-09 10:54:27 +0530

    Fixes #2060: Adds support for additional paper sizes (#2061)
    Adds support for paper sizes - A0 and A1
    
    Signed-off-by: Abishek V Ashok<abishekvashok@gmail.com>

--- api/utils/dimen.py
+++ api/utils/dimen.py
@@ -9,9 +9,15 @@ badge_config = {}
 
 
 def init_dimen():
-    paper_sizes = ['A2', 'A3', 'A4']
+    paper_sizes = ['A0', 'A1', 'A2', 'A3', 'A4']
     for paper in paper_sizes:
-        if paper == 'A2':
+        if paper == 'A0':
+            badge_config.__setitem__(paper, {'4x3': Dimen(72, '4x3', paper)})
+            badge_config[paper]['4.5x4'] = Dimen(2, '4.5x4', paper)
+        elif paper == 'A1':
+            badge_config.__setitem__(paper, {'4x3': Dimen(36, '4x3', paper)})
+            badge_config[paper]['4.5x4'] = Dimen(2, '4.5x4', paper)
+        elif paper == 'A2':
             badge_config.__setitem__(paper, {'4x3': Dimen(18, '4x3', paper)})
             badge_config[paper]['4.5x4'] = Dimen(15, '4.5x4', paper)
         elif paper == 'A3':
