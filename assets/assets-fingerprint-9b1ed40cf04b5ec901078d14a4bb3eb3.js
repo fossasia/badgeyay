fecec5bac96436dbc8a34232019e0a3179d77817
Travis CI <noreply+travis@fossasia.org>
2019-10-27 12:11:55 +0000

    Travis CI Clean Deploy 8f8a402

--- assets/assets-fingerprint-9b1ed40cf04b5ec901078d14a4bb3eb3.js
+++
@@ -1,1 +0,0 @@
-(function(a){a.ASSET_FINGERPRINT_HASH="-9b1ed40cf04b5ec901078d14a4bb3eb3"})(window);
---
+++ assets/assets-fingerprint-ccaddefc3c2959a754d1c9c9df6b62af.js
@@ -0,0 +1,1 @@
+(function(a){a.ASSET_FINGERPRINT_HASH="-ccaddefc3c2959a754d1c9c9df6b62af"})(window);
