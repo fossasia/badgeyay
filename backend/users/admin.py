75511393841d4966b35962eb1fc63fc9b3e42791
PragatiVerma18 <itispragativerma.com>
2020-01-26 13:19:18 +0530

    fixed admin panel

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import User
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 # Register your models here.
+
+admin.site.register(User, BaseUserAdmin)
+
+admin.site.unregister(Group)
